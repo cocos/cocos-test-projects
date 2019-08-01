ca238114e299b3662df3f7d56e5af3d321642e13
JayceLai <goodlaijiaxin@qq.com>
2019-08-01 18:56:10 +0800

    fix deprecated testing

--- assets/cases/base/deprecated-testing.ts
+++ assets/cases/base/deprecated-testing.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, SpriteFrame, AnimationComponent, AnimationClip, misc, macro, Texture2D } from "cc";
+import { _decorator, Component, Node, SpriteFrame, AnimationComponent, AnimationClip, macro, Texture2D, math } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("deprecated-testing")
@@ -6,24 +6,12 @@ export class deprecatedtesting extends Component {
 
     start () {
 
-        // deprecated testing
-        let anicom = new AnimationComponent();
-        let clip = new AnimationClip();
-        anicom.addClip(clip, 'clip');
-        anicom.getAnimationState('clip');
-
-        // remove testing
-        cc.misc.clampf();
-        cc.misc.lerp();
-        //@ts-ignore
-        misc.clampf();
-        //@ts-ignore
-        misc.lerp();
-
-        //warning testing
-        let tex2d = new Texture2D();
-        tex2d.releaseTexture();
-        tex2d.getHtmlElementObj();
+        let anim = this.node.addComponent(AnimationComponent);
+        let clip = new AnimationClip('DD');
+        // API 更名
+        console.log(anim.addClip(clip));
+        // API 更名 + 参数不兼容
+        console.log(anim.removeClip(clip));
 
     }
 
