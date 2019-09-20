08f69fd8b02a4f6e4b6a9fba42240498154c5011
jiaxin.lai <goodlaijiaxin@qq.com>
2019-09-20 14:24:38 +0800

    add desc & add eulerAngle linear lerp (#49)

--- assets/cases/tween/tween-test.ts
+++ assets/cases/tween/tween-test.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Vec3, tweenUtil } from "cc";
+import { _decorator, Component, Vec3, tweenUtil, Quat } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("tween-test")
@@ -6,6 +6,7 @@ export class tweentest extends Component {
 
     _wpos: Vec3 = new Vec3(0, 0, 0);
     _wscale: Vec3 = new Vec3(1, 1, 1);
+    _leuler: Vec3 = new Vec3(0, 0, 0);
 
     start () {
         // Your initialization goes here.
@@ -26,11 +27,20 @@ export class tweentest extends Component {
             .union()
             .repeat(Infinity)
             .start();
+
+        Vec3.copy(this._leuler, this.node.eulerAngles);
+
+        tweenUtil(this._leuler)
+            .to(4.5, new Vec3(360, 360, 360), { easing: 'Bounce-InOut' })
+            .to(4.5, new Vec3(0, 0, 0), { easing: 'Elastic-Out' })
+            .union()
+            .repeat(Infinity)
+            .start();
     }
 
-    update (deltaTime: number) {
-        // Your update function goes here.
+    update () {
         this.node.worldPosition = this._wpos;
         this.node.worldScale = this._wscale;
+        this.node.eulerAngles = this._leuler;
     }
 }
