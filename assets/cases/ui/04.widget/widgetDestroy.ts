86f8450243667c3424971078a035f71014c71a1e
ChiaNing <sun.cloud.mo@gmail.com>
2019-11-14 12:45:33 +0800

    widget destroy case add (#87)

---
+++ assets/cases/ui/04.widget/widgetDestroy.ts
@@ -0,0 +1,39 @@
+import { _decorator, Component, Node, Prefab, Vec3, LabelAtlas, LabelComponent } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("widgetDestroy")
+export class widgetDestroy extends Component {
+
+    @property ({type:Prefab})
+    defaultPre = null;
+
+    @property ({type:LabelComponent})
+    coinNumber: LabelComponent = null;
+
+    @property ({type:LabelComponent})
+    activeWidgetNum = null;
+
+    movePos = new Vec3(-200, 0, 0);
+
+    createprefab() {
+        let item = cc.instantiate(this.defaultPre);
+        this.node.addChild(item);
+        this.schedule(this.updateLabel,0.5);
+    }
+
+    destroyThenCreate() {
+        if (this.node.children.length < 1) {return;}
+        this.node.children[this.node.children.length - 1].destroy();
+        this.createprefab();
+    }
+
+    moveRoot() {
+        this.movePos.x += 20;
+        this.node.setPosition(this.movePos);
+    }
+
+    updateLabel() {
+        this.coinNumber.string = ('The Coin Num is:' + cc.director._scene.children[2].children[3].children.length);
+        this.activeWidgetNum.string = 'The active Widget Num is:' + (cc._widgetManager._activeWidgetsIterator.length - 4);
+    }
+}
