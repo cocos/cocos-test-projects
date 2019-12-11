cc981f106d3ce4222447e350210db4250429ec70
ftershock <53322920+ftershock@users.noreply.github.com>
2019-12-11 14:01:10 +0800

    修改tips和按钮功能 (#114)
    修改tips和按钮功能

--- assets/cases/ui/17.sprite-atlas/TS/Test.ts
+++ assets/cases/ui/17.sprite-atlas/TS/Test.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, director, LabelComponent, SpriteComponent, EditBoxComponent, SpriteFrame } from "cc";
+import { _decorator, Component, director, LabelComponent, SpriteComponent, EditBoxComponent, SpriteFrame, Vec3 } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("Test")
@@ -9,6 +9,15 @@ export class Test extends Component {
 
     @property({type:SpriteFrame})
     sf=null;
+
+    @property({type:SpriteFrame})
+    sea=null;
+
+    @property({type:SpriteFrame})
+    lake=null;
+
+    @property({type:SpriteFrame})
+    mountain=null;
 
     private tiplabel=null;
     private showlabel=null;
@@ -47,8 +56,29 @@ export class Test extends Component {
         this.showlabel.getComponent(LabelComponent).string='清除图集';
         this._label='替换失败';
     }
-    button3(){
-        this._sprite.spriteFrame=this.sf;
-        this.showlabel.getComponent(LabelComponent).string='更换图片 tree';
+    button3(name:string){
+        this.name = this.editbox.string;
+        if(this.name == 'tree'){
+            this._sprite.spriteFrame=this.sf;      
+            this.showlabel.getComponent(LabelComponent).string='更换图片 tree';
+        }
+        if(this.name == 'sea'){
+            this._sprite.spriteFrame=this.sea;
+            this.showlabel.getComponent(LabelComponent).string='更换图片 sea';
+        }
+        if(this.name == 'lake'){
+            this._sprite.spriteFrame=this.lake;
+            this.showlabel.getComponent(LabelComponent).string='更换图片 lake';
+        }
+        if(this.name == 'mountain'){
+            this._sprite.spriteFrame=this.mountain;
+            this.showlabel.getComponent(LabelComponent).string='更换图片 mountain';
+        }
+        if(this.name != 'mountain' && this.name != 'lake' && this.name != 'sea' && this.name != 'tree'){
+            this.showlabel.getComponent(LabelComponent).string='请输入正确的名字';
+        }
+    }
+    button4(){
+        this.tiplabel.position = new Vec3(90,72,0);
     }
 }
