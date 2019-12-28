d21f0616ef7e8e36125c711844909ed4c868459f
JayceLai <goodlaijiaxin@qq.com>
2019-12-28 15:08:48 +0800

    physics checker tweaks

--- assets/cases/physics/scripts/PhysicsEnvCheck.ts
+++ assets/cases/physics/scripts/PhysicsEnvCheck.ts
@@ -14,47 +14,75 @@ Enum(EPhysicsItem);
 @menu("physics/PhysicsEnvCheck")
 export class PhysicsEnvCheck extends Component {
 
-    @property({ type: EPhysicsItem })
+    // @property({ type: EPhysicsItem })
     physics: EPhysicsItem = EPhysicsItem.CANNON_AMMO;
 
-    start () {
+    onLoad () {
+        if (CC_PHYSICS_BUILTIN) {
+            const lbCom = this.node.getChildByName('desc').getComponent(LabelComponent);
+            lbCom.string = "当前物理：builtin";
+        } else if (CC_PHYSICS_CANNON) {
+            const lbCom = this.node.getChildByName('desc').getComponent(LabelComponent);
+            lbCom.string = "当前物理：cannon.js";
+        } else if (CC_PHYSICS_AMMO) {
+            const lbCom = this.node.getChildByName('desc').getComponent(LabelComponent);
+            lbCom.string = "当前物理：ammo.js";
+        } else {
+            const lbCom = this.node.getChildByName('desc').getComponent(LabelComponent);
+            lbCom.string = "当前物理：none";
+        }
+
+        const name = this.node.name;
+        if (name == "cannon-ammo") {
+            this.physics = EPhysicsItem.CANNON_AMMO;
+        } else if (name == "builtin") {
+            this.physics = EPhysicsItem.BUILTIN;
+        } else if (name == "cannon") {
+            this.physics = EPhysicsItem.CANNON;
+        } else if (name == "ammo") {
+            this.physics = EPhysicsItem.AMMO;
+        } else if (name == "builtin-cannon-ammo") {
+            this.physics = EPhysicsItem.ALL;
+        }
+
         switch (this.physics) {
             case EPhysicsItem.ALL:
-                return;
+                break;
             case EPhysicsItem.CANNON_AMMO:
                 if (CC_PHYSICS_CANNON || CC_PHYSICS_AMMO) {
-                    return;
+                    break;
                 }
 
-                let lbCom = this.getComponentInChildren(LabelComponent);
+                let lbCom = this.node.getChildByName('lb').getComponent(LabelComponent);
+                lbCom.enabled = true;
                 lbCom.string = "测试此场景需要将物理模块设置为 cannon.js 或 ammo.js";
-                let sprCom = this.getComponent(SpriteComponent);
+                let sprCom = this.getComponentInChildren(SpriteComponent);
                 sprCom.enabled = true;
                 break;
             case EPhysicsItem.CANNON:
                 if (!CC_PHYSICS_CANNON) {
-                    let lbCom = this.getComponentInChildren(LabelComponent);
+                    let lbCom = this.node.getChildByName('lb').getComponent(LabelComponent);
+                    lbCom.enabled = true;
                     lbCom.string = "测试此场景需要将物理模块设置为 cannon.js";
-
-                    let sprCom = this.getComponent(SpriteComponent);
+                    let sprCom = this.getComponentInChildren(SpriteComponent);
                     sprCom.enabled = true;
                 }
                 break;
             case EPhysicsItem.AMMO:
                 if (!CC_PHYSICS_AMMO) {
-                    let lbCom = this.getComponentInChildren(LabelComponent);
+                    let lbCom = this.node.getChildByName('lb').getComponent(LabelComponent);
+                    lbCom.enabled = true;
                     lbCom.string = "测试此场景需要将物理模块设置为 ammo.js";
-
-                    let sprCom = this.getComponent(SpriteComponent);
+                    let sprCom = this.getComponentInChildren(SpriteComponent);
                     sprCom.enabled = true;
                 }
                 break;
             case EPhysicsItem.BUILTIN:
                 if (!CC_PHYSICS_BUILTIN) {
-                    let lbCom = this.getComponentInChildren(LabelComponent);
+                    let lbCom = this.node.getChildByName('lb').getComponent(LabelComponent);
+                    lbCom.enabled = true;
                     lbCom.string = "测试此场景需要将物理模块设置为 builtin";
-
-                    let sprCom = this.getComponent(SpriteComponent);
+                    let sprCom = this.getComponentInChildren(SpriteComponent);
                     sprCom.enabled = true;
                 }
                 break;
