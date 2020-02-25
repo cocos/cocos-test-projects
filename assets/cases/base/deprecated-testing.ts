5445bccd3273e4e0a12aeade25b6bc1812b10683
JayceLai <goodlaijiaxin@qq.com>
2020-02-25 14:25:03 +0800

    fix deprecated test at alipay

--- assets/cases/base/deprecated-testing.ts
+++ assets/cases/base/deprecated-testing.ts
@@ -5,14 +5,6 @@ const { ccclass, property } = _decorator;
 export class deprecatedtesting extends Component {
 
     start () {
-
-        let anim = this.node.addComponent(AnimationComponent);
-        let clip = new AnimationClip('DD');
-        // API 更名
-        console.log(anim.addClip(clip));
-        // API 更名 + 参数不兼容
-        console.log(anim.removeClip(clip));
-
         // API 更名
         tweenUtil({});
     }
