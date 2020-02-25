c942af9aab4feac4a7ea3f244337cc00e4ae09ec
jiaxin.lai <goodlaijiaxin@qq.com>
2020-02-25 15:21:37 +0800

    fix deprecated test at alipay (#180)
    * fix deprecated test at alipay
    
    * tweaks

--- assets/cases/base/deprecated-testing.ts
+++ assets/cases/base/deprecated-testing.ts
@@ -5,13 +5,15 @@ const { ccclass, property } = _decorator;
 export class deprecatedtesting extends Component {
 
     start () {
-
-        let anim = this.node.addComponent(AnimationComponent);
-        let clip = new AnimationClip('DD');
-        // API 更名
-        console.log(anim.addClip(clip));
-        // API 更名 + 参数不兼容
-        console.log(anim.removeClip(clip));
+        // TODO: 支付宝平台上有问题
+        if (!CC_ALIPAY) {
+            let anim = this.node.addComponent(AnimationComponent);
+            let clip = new AnimationClip('DD');
+            // API 更名
+            console.log(anim.addClip(clip));
+            // API 更名 + 参数不兼容
+            console.log(anim.removeClip(clip));
+        }
 
         // API 更名
         tweenUtil({});
