b54f657a3cc7da6a134569e90b9d1579130a8e6e
JayceLai <goodlaijiaxin@qq.com>
2020-04-13 14:12:37 +0800

    add physics sync test case

---
+++ assets/cases/physics/scripts/physics-sync-test.ts
@@ -0,0 +1,24 @@
+import { _decorator, Component, Node, PhysicsSystem } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('physicsSyncTest')
+export class physicsSyncTest extends Component {
+
+    count = 0;
+
+    // @property
+
+    start () {
+        PhysicsSystem.instance.enable = false;
+    }
+
+    update (dt: number) {
+        if (this.count == 1) {
+            this.node.setWorldPosition(0, 0.5, 0);
+        } else if (this.count == 60) {
+            PhysicsSystem.instance.enable = true;
+        }
+
+        this.count++;
+    }
+}
