1353e84995f14b9b158a907f25d925ab6a81273c
ChiaNing <sun.cloud.mo@gmail.com>
2020-04-24 10:25:26 +0800

    builtIn asset release protect

--- assets/cases/scripting/asset_loading/LoadResDir/LoadResDir_example.ts
+++ assets/cases/scripting/asset_loading/LoadResDir/LoadResDir_example.ts
@@ -1,6 +1,18 @@
 import { _decorator, Component, Node, Prefab, instantiate, LabelComponent, SpriteFrame, loader, js, JsonAsset, ScrollViewComponent } from "cc";
 const { ccclass, property } = _decorator;
 
+const builtInEffectList = [
+    '711ebe11-f673-4cd9-9a83-63c60ba54c5b.json',
+    '971bdb23-3ff6-43eb-b422-1c30165a3663.json',
+    '17debcc3-0a6b-4b8a-b00b-dc58b885581e.json',
+    'd1346436-ac96-4271-b863-1f4fdead95b0.json',
+    '60f7195c-ec2a-45eb-ba94-8955f60e81d0.json',
+    '1baf0fc9-befa-459c-8bdd-af1a450a0319.json',
+    '1d08ef62-a503-4ce2-8b9a-46c90873f7d3.json',
+    'a7612b54-35e3-4238-a1a9-4a7b54635839.json',
+    'a3cd009f-0ab0-420d-9278-b9fdab939bbc.json',
+];
+
 @ccclass("LoadResDirexample")
 export class LoadResDirexample extends Component {
     /* class member could be defined like this */
@@ -42,9 +54,25 @@ export class LoadResDirexample extends Component {
             const asset = this._assets[i];
             // 需要释放所有资源依赖
             const deps = loader.getDependsRecursively(asset);
+            this._removeBuiltInEffect(deps);
             loader.release(deps);
         }
         this._assets = [];
+    }
+
+    _removeBuiltInEffect (deps) {
+        let cache = [];
+        for (let  i = 0; i < deps.length; i++) {
+            for (let j = 0; j < builtInEffectList.length; j++) {
+                if (deps[i].includes(builtInEffectList[j])){
+                    cache.push(i);
+                }
+            }
+        }
+        for(let k = 0; k < cache.length; k++) {
+            delete deps[cache[k]];
+        }
+        cache = [];
     }
  
     onClearAll () {
