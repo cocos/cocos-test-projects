8962df264eb97a3f5da2e178707118b8b21ff555
ChiaNing <sun.cloud.mo@gmail.com>
2020-04-30 10:26:01 +0800

    webSocket update (#227)

--- assets/cases/network/NetworkCtrl.ts
+++ assets/cases/network/NetworkCtrl.ts
@@ -1,18 +1,18 @@
-import { _decorator, Component, Node, SpriteComponent, ProgressBarComponent, CCObject , LabelComponent, Asset, assert, Socket } from "cc";
+import { _decorator, Component, LabelComponent, Asset, assert, loader } from 'cc';
 const { ccclass, property } = _decorator;
 
-//imported from socket-io.js
+// imported from socket-io.js
 declare var io:any;
 
-@ccclass("NetworkCtrl")
+@ccclass('NetworkCtrl')
 export class NetworkCtrl extends Component {
 
     @property({type: LabelComponent})
     xhr: LabelComponent = null;
-    
+
     @property({type: LabelComponent})
     xhrAB: LabelComponent = null;
-    
+
     @property({type: LabelComponent})
     xhrTimeout: LabelComponent = null;
 
@@ -30,7 +30,7 @@ export class NetworkCtrl extends Component {
     private _wsiSendBinary: WebSocket = null;
     private _sioClient: any = null;
 
-    private tag: string = "";
+    private tag: string = '';
 
     // use this for initialization
     start () {
@@ -38,12 +38,12 @@ export class NetworkCtrl extends Component {
         this._xhrXHR = null;
         this._xhrHRAB = null;
         this._xhrXHRTimeout = null;
-        
-        this.xhr.string = "waiting..";
-        this.xhrAB.string = "waiting..";
-        this.xhrTimeout.string = "waiting..";
-        this.websocket.string = "waiting..";
-        
+
+        this.xhr.string = 'waiting..';
+        this.xhrAB.string = 'waiting..';
+        this.xhrTimeout.string = 'waiting..';
+        this.websocket.string = 'waiting..';
+
         this.sendXHR();
         this.sendXHRAB();
         this.sendXHRTimeout();
@@ -51,7 +51,7 @@ export class NetworkCtrl extends Component {
     }
 
     onDestroy () {
-        var wsiSendBinary = this._wsiSendBinary;
+        let wsiSendBinary = this._wsiSendBinary;
         if (wsiSendBinary) {
             wsiSendBinary.onopen = null;
             wsiSendBinary.onmessage = null;
@@ -63,14 +63,14 @@ export class NetworkCtrl extends Component {
         this.rmXhrEventListener(this._xhrHRAB);
         this.rmXhrEventListener(this._xhrXHRTimeout);
     }
-    
+
     sendXHR () {
-        var xhr = cc.loader.getXMLHttpRequest();
+        let xhr = cc.loader.getXMLHttpRequest();
         this.streamXHREventsToLabel(xhr, this.xhr, 'GET');
 
-        xhr.open("GET", "https://httpbin.org/get?show_env=1", true);
+        xhr.open('GET', 'https://httpbin.org/get?show_env=1', true);
         if (cc.sys.isNative) {
-            xhr.setRequestHeader("Accept-Encoding","gzip,deflate");
+            xhr.setRequestHeader('Accept-Encoding','gzip,deflate');
         }
 
         // note: In Internet Explorer, the timeout property may be set only after calling the open()
@@ -82,22 +82,22 @@ export class NetworkCtrl extends Component {
     }
 
     sendXHRAB () {
-        var xhr = cc.loader.getXMLHttpRequest();
-        this.streamXHREventsToLabel(xhr, this.xhrAB, "POST");
+        let xhr = cc.loader.getXMLHttpRequest();
+        this.streamXHREventsToLabel(xhr, this.xhrAB, 'POST');
 
-        xhr.open("POST", "https://httpbin.org/post");
-        //set Content-type "text/plain" to post ArrayBuffer or ArrayBufferView
-        xhr.setRequestHeader("Content-Type","text/plain");
+        xhr.open('POST', 'https://httpbin.org/post');
+        // set Content-type "text/plain" to post ArrayBuffer or ArrayBufferView
+        xhr.setRequestHeader('Content-Type','text/plain');
         // Uint8Array is an ArrayBufferView
         xhr.send(new Uint8Array([1,2,3,4,5]));
         this._xhrHRAB = xhr;
     }
-    
+
     sendXHRTimeout () {
-        var xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
         this.streamXHREventsToLabel(xhr, this.xhrTimeout, 'GET');
 
-        xhr.open("GET", "https://192.168.22.222", true);
+        xhr.open('GET', 'https://192.168.22.222', true);
 
         // note: In Internet Explorer, the timeout property may be set only after calling the open()
         // method and before calling the send() method.
@@ -105,91 +105,93 @@ export class NetworkCtrl extends Component {
         xhr.send();
         this._xhrXHRTimeout = xhr;
     }
-    
+
     prepareWebSocket () {
-        var self = this;
-        var websocketLabel = this.websocket.node.getParent().getComponent(LabelComponent);
-        var respLabel = this.websocket;
+        const self = this;
+        const websocketLabel = this.websocket.node.getParent().getComponent(LabelComponent);
+        const respLabel = this.websocket;
         // We should pass the cacert to libwebsockets used in native platform, otherwise the wss connection would be closed.
-        this._wsiSendBinary = new (WebSocket as any)("wss://echo.websocket.org", [], this.wssCacert.nativeUrl);
-        this._wsiSendBinary.binaryType = "arraybuffer";
-        this._wsiSendBinary.onopen = function(evt) {
-            respLabel.string = "Opened!";
-            websocketLabel.string = "WebSocket: onopen"
+        let url = this.wssCacert.nativeUrl;
+        if (loader.md5Pipe) {
+            url = loader.md5Pipe.transformURL(url);
+        }
+        this._wsiSendBinary = new (WebSocket as any)('wss://echo.websocket.org', [], url);
+        this._wsiSendBinary.binaryType = 'arraybuffer';
+        this._wsiSendBinary.onopen = function (evt) {
+            respLabel.string = 'Opened!';
+            websocketLabel.string = 'WebSocket: onopen'
         };
 
-        this._wsiSendBinary.onmessage = function(evt) {
-            var binary = new Uint8Array(evt.data);
-            var binaryStr = 'response bin msg: ';
+        this._wsiSendBinary.onmessage = function (evt) {
+            const binary = new Uint8Array(evt.data);
+            let binaryStr = 'response bin msg: ';
 
-            var str = '0x';
-            let hexMap = '0123456789ABCDEF'.split("");
+            let str = '0x';
+            const hexMap = '0123456789ABCDEF'.split('');
             assert(hexMap.length == 16);
 
 
-            for (var i = 0; i < binary.length; i++) {
+            for (let i = 0; i < binary.length; i++) {
                str += hexMap[binary[i] >> 4];
                str += hexMap[binary[i] & 0x0F];
             }
 
             binaryStr += str;
             respLabel.string = binaryStr;
-            websocketLabel.string = "WebSocket: onmessage"
+            websocketLabel.string = 'WebSocket: onmessage'
         };
 
-        this._wsiSendBinary.onerror = function(evt) {
-           websocketLabel.string = "WebSocket: onerror"
-            respLabel.string = "Error!";
+        this._wsiSendBinary.onerror = function (evt) {
+           websocketLabel.string = 'WebSocket: onerror'
+            respLabel.string = 'Error!';
         };
 
-        this._wsiSendBinary.onclose = function(evt) {
-            websocketLabel.string = "WebSocket: onclose"
-            // After close, it's no longer possible to use it again, 
+        this._wsiSendBinary.onclose = function (evt) {
+            websocketLabel.string = 'WebSocket: onclose'
+            // After close, it's no longer possible to use it again,
             // if you want to send another request, you need to create a new websocket instance
             self._wsiSendBinary = null;
-            respLabel.string = "Close!";
+            respLabel.string = 'Close!';
         };
-        
+
         this.scheduleOnce(this.sendWebSocketBinary, 1);
     }
 
-    sendWebSocketBinary(sender) {
-        var websocketLabel = this.websocket.node.getParent().getComponent(LabelComponent);
+    sendWebSocketBinary (sender) {
+        let websocketLabel = this.websocket.node.getParent().getComponent(LabelComponent);
         if (!this._wsiSendBinary) { return; }
-        if (this._wsiSendBinary.readyState === WebSocket.OPEN)
-        {
-            websocketLabel.string = "WebSocket: sendbinary";
-            var buf = "Hello WebSocket中文,\0 I'm\0 a\0 binary\0 message\0.";
-            
-            var arrData = new Uint16Array(buf.length);
-            for (var i = 0; i < buf.length; i++) {
+        if (this._wsiSendBinary.readyState === WebSocket.OPEN){
+            websocketLabel.string = 'WebSocket: sendbinary';
+            let buf = 'Hello WebSocket中文,\0 I\'m\0 a\0 binary\0 message\0.';
+
+            let arrData = new Uint16Array(buf.length);
+            for (let i = 0; i < buf.length; i++) {
                 arrData[i] = buf.charCodeAt(i);
             }
-            
+
             this._wsiSendBinary.send(arrData.buffer);
         }
-        else
-        {
-            var warningStr = "send binary websocket instance wasn't ready...";
-            websocketLabel.string = "WebSocket: not ready";
+        else{
+            let warningStr = 'send binary websocket instance wasn\'t ready...';
+            websocketLabel.string = 'WebSocket: not ready';
             this.websocket.string = warningStr;
             this.scheduleOnce(function () {
                 this.sendWebSocketBinary();
             }, 1);
         }
     }
-    
+
     streamXHREventsToLabel ( xhr:XMLHttpRequest, label:LabelComponent, method:string, responseHandler?:(string)=>string ) {
-        var handler = responseHandler || function (response) {
-            return method + " Response (30 chars): " + response.substring(0, 30) + "...";
+        let handler = responseHandler || function (response) {
+            return method + ' Response (30 chars): ' + response.substring(0, 30) + '...';
         };
-        
-        let eventLabel = label.node.getParent().getComponent(LabelComponent);
-        var eventLabelOrigin = eventLabel.string;
+
+        const eventLabel = label.node.getParent().getComponent(LabelComponent);
+        let eventLabelOrigin = eventLabel.string;
         // Simple events
         ['loadstart', 'abort', 'error', 'load', 'loadend', 'timeout'].forEach(function (eventname) {
-            xhr["on" + eventname] = function () {
-                eventLabel.string = eventLabelOrigin + "\nEvent : " + eventname;
+            xhr['on' + eventname] = function () {
+                eventLabel.string = eventLabelOrigin + '\nEvent : ' + eventname;
                 if (eventname === 'timeout') {
                     label.string += '(timeout)';
                 }
@@ -198,26 +200,26 @@ export class NetworkCtrl extends Component {
                 }
             };
         });
-    
+
         // Special event
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status >= 200) {
                 label.string = handler(xhr.responseText);
             }
             else if (xhr.status === 404) {
-                label.string = "404 page not found!"
+                label.string = '404 page not found!'
             }
             else if (xhr.readyState === 3) {
-                label.string = "Request dealing!";
+                label.string = 'Request dealing!';
             }
             else if (xhr.readyState === 2) {
-                label.string = "Request received!";
+                label.string = 'Request received!';
             }
             else if (xhr.readyState === 1) {
-                label.string = "Server connection established! Request hasn't been received";
+                label.string = 'Server connection established! Request hasn\'t been received';
             }
             else if (xhr.readyState === 0) {
-                label.string = "Request hasn't been initiated!";
+                label.string = 'Request hasn\'t been initiated!';
             }
         };
     }
@@ -227,7 +229,7 @@ export class NetworkCtrl extends Component {
             return;
         }
         ['loadstart', 'abort', 'error', 'load', 'loadend', 'timeout'].forEach(function (eventname) {
-            xhr["on" + eventname] = null;
+            xhr['on' + eventname] = null;
         });
         xhr.onreadystatechange = null;
     }
