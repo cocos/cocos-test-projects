6f8efd28041efe914660a4e3001c45a5dc088329
ChiaNing <sun.cloud.mo@gmail.com>
2020-07-21 18:28:52 +0800

    multiTouch case update (#263)

--- assets/cases/event/multiTouch/MultiTouchCtrl.ts
+++ assets/cases/event/multiTouch/MultiTouchCtrl.ts
@@ -1,50 +1,51 @@
-import { _decorator, Component, systemEvent, SystemEventType, EventTouch, LabelComponent, ToggleComponent, Node, macro } from "cc";
+import { _decorator, Component, EventTouch, ToggleComponent, Node, macro } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("MultiTouchCtrl")
 export class MultiTouchCtrl extends Component {
 
-    /* use `property` decorator if your want the member to be serializable */
-    // @property
-    // serializableDummy = 0;
-    @property(LabelComponent)
-    labelShow: LabelComponent = null;
-
     @property(ToggleComponent)
     toggle: ToggleComponent = null;
 
+    @property(Node)
+    canvas: Node = null;
+
+    @property(Node)
+    target: Node = null;
+
     start () {
-        // Your initialization goes here.
-        systemEvent.on(SystemEventType.TOUCH_START, this.onTouchStart, this);
-        systemEvent.on(SystemEventType.TOUCH_END, this.onTouchEnd, this);
-        systemEvent.on(SystemEventType.TOUCH_CANCEL, this.onTouchEnd, this);
+        this.canvas.on(Node.EventType.TOUCH_MOVE,this.onTouchMove, this);
     }
 
-    onDestroy() {
-        systemEvent.off(SystemEventType.TOUCH_START, this.onTouchStart, this);
-        systemEvent.off(SystemEventType.TOUCH_END, this.onTouchEnd, this);
-        systemEvent.off(SystemEventType.TOUCH_CANCEL, this.onTouchEnd, this);
+    onDestroy () {
+        this.canvas.off(Node.EventType.TOUCH_MOVE,this.onTouchMove, this);
     }
 
-    onTouchStart(touch: Touch, event: EventTouch) {
-        let touches = event.getTouches();
-
-        for(let i = 0; i < touches.length; i++ ) {
-            let touch = touches[i];
-            this.labelShow.string += `TouchStart: ${touch.getLocation()} \n`;
-        }
-    }
-
-    onTouchEnd(touch: Touch, event: EventTouch) {
-        this.labelShow.string = 'TouchEnd';
-    }
-
-    changeMulit() {
-        this.labelShow.string = '';
+    changeMulit () {
         if (this.toggle.isChecked) {
             macro.ENABLE_MULTI_TOUCH = true;
         } else {
             macro.ENABLE_MULTI_TOUCH = false;
+        }
+    }
+
+    onTouchMove (event: EventTouch) {
+        const touches = event.getTouches();
+        if (macro.ENABLE_MULTI_TOUCH && touches.length > 1) {
+            const touch1 = touches[0];
+            const touch2 = touches[1];
+            const delta1 = touch1.getDelta();
+            const delta2 = touch2.getDelta();
+            const touchPoint1 = touch1.getLocation();
+            const touchPoint2 = touch2.getLocation();
+            const distance = touchPoint1.subtract(touchPoint2);
+            const delta = delta1.subtract(delta2);
+            if (Math.abs(distance.x) > Math.abs(distance.y)) {
+                this.target.setScale((distance.x + delta.x) / distance.x * this.target.getScale().x, this.target.getScale().y, 1);
+            }
+            else {
+                this.target.setScale(this.target.getScale().x, (distance.y + delta.y) / distance.y * this.target.getScale().y, 1);
+            }
         }
     }
 }
