4883b6f7a18ef8a0ce4500e68892a0706b661511
SB <leslieleigh@outlook.com>
2020-10-20 16:43:57 +0800

    Build time constants test-cases

---
+++ assets/cases/scripting/build-time-constants-test/BuildTimeConstantsTest.ts
@@ -0,0 +1,31 @@
+
+import * as buildTimeConstants from 'cce.env';
+import * as cc from 'cc';
+
+// import * as buildTimeConstants from 'build-time-constants';
+
+const keys = (Object.keys(buildTimeConstants) as (keyof typeof buildTimeConstants)[]).sort();
+
+@cc._decorator.ccclass('BuildTimeConstantsTest')
+@cc._decorator.menu('TestCases/Scripting/BuildTimeConstantsTest')
+@cc._decorator.executeInEditMode
+export class BuildTimeConstantsTest extends cc.Component {
+    @cc._decorator.property(cc.Node)
+    public labelNode: cc.Node = null;
+    
+    public start () {
+        const labelComponent = this.labelNode.getComponent(cc.Label);
+        const keyNameMaxLen = keys.reduce((len, key) => Math.max(len, key.length), 0);
+        labelComponent.string = `\
+${
+    keys.map((key) => {
+        const value = buildTimeConstants[key];
+        const valueRep = typeof value === 'boolean' ?
+            (value ? '✔️' : '❌'):
+            value;
+        return `${key.padStart(keyNameMaxLen, ' ')} : ${valueRep}`;
+    }).join('\n')
+}
+`;
+    }
+}
