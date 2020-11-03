9d4c0e1aa567b2a4922c207af960c7e76057ae0b
Knox <knox.h@qq.com>
2020-11-03 11:24:18 +0800

    improve video player and web view (#324)
    * improve video player and web view
    
    open ENABLE_TRANSPARENT_CANVAS
    
    * refine code

--- assets/cases/ui/22.webview/webview-ctrl.ts
+++ assets/cases/ui/22.webview/webview-ctrl.ts
@@ -1,29 +1,45 @@
-import { _decorator, Component, Label, WebView, TextAsset } from 'cc';
-const { ccclass, property } = _decorator;
-
-function getEventType(type: WebView.EventType) {
-    switch (type) {
-        case WebView.EventType.LOADING:
-            return 'LOADING';
-        case WebView.EventType.LOADED:
-            return 'LOADED';
-        case WebView.EventType.ERROR:
-            return 'Error';
-    }
-}
+import {_decorator, Component, Label, WebView, TextAsset, Node, sys} from 'cc';
+const { ccclass, type } = _decorator;
 
 @ccclass('WebviewCtrl')
 export class WebviewCtrl extends Component {
-    @property(WebView)
+    @type(WebView)
     webview = null;
-    @property(Label)
+    @type(Label)
     eventTips = null;
+    @type(Node)
+    noSupport = null;
+
+    start () {
+        // 隐藏不支持 video player 的平台
+        switch (sys.platform) {
+            case sys.ANDROID:
+            case sys.OS_IOS:
+            case sys.MACOS:
+            case sys.ALIPAY_MINI_GAME:
+            case sys.BYTEDANCE_MINI_GAME:
+            case sys.COCOSPLAY:
+            case sys.HUAWEI_QUICK_GAME:
+            case sys.OPPO_MINI_GAME:
+            case sys.VIVO_MINI_GAME:
+            case sys.XIAOMI_GAME:
+            case sys.XIAOMI_QUICK_GAME:
+            case sys.BAIDU_MINI_GAME:
+            case sys.WECHAT_GAME:
+            case sys.LINKSURE:
+            case sys.QTT_GAME:
+            case sys.WIN32:
+                this.noSupport.active = true;
+                this.webview.node.active = false;
+                break;
+        }
+    }
 
     onGoTo () {
         this.webview.url = 'https://www.baidu.com';
     }
 
     onEventTypes (target, eventType: WebView.EventType) {
-        this.eventTips.string = '触发事件：' + getEventType(eventType);
+        this.eventTips.string = '触发事件：' + eventType;
     }
 }
