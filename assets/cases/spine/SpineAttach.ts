8ea2881fd52d27eebf198fd785a74684d285873b
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2020-11-12 13:48:29 +0800

    add spine demos

---
+++ assets/cases/spine/SpineAttach.ts
@@ -0,0 +1,27 @@
+
+
+
+import { _decorator, Component, sp, Size, Prefab, Label } from 'cc';
+const { ccclass, property } = _decorator;
+
+
+@ccclass('SpineAttach')
+export default class extends Component {
+
+    @property({ type: sp.Skeleton })
+    skeleton: sp.Skeleton | null = null;
+
+    @property({ type: Label })
+    modeLabel: Label | null = null;
+
+    changeMode () {
+        let isCached = this.skeleton!.isAnimationCached();
+        if (isCached) {
+            this.skeleton!.setAnimationCacheMode(sp.Skeleton.AnimationCacheMode.REALTIME);
+            this.modeLabel!.string = "cache";
+        } else {
+            this.skeleton!.setAnimationCacheMode(sp.Skeleton.AnimationCacheMode.SHARED_CACHE);
+            this.modeLabel!.string = "realtime";
+        }
+    }
+}
