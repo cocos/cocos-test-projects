4ddafa0bfc30be895e24831b890d8c912c08aaff
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2020-11-19 22:50:07 +0800

    save

---
+++ assets/cases/dragonbones/DragonBonesAttach.ts
@@ -0,0 +1,89 @@
+
+import { _decorator, Component, director, instantiate, Label, Color, Node, Prefab, dragonBones, Sprite } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('DragonBonesAttach')
+export default class extends Component {
+
+    @property({type: dragonBones.ArmatureDisplay})
+    skeleton: dragonBones.ArmatureDisplay |null = null;
+
+    @property({type: Prefab})
+    targetPrefab: Prefab | null = null;
+
+    @property({type: Label})
+    modeLabel: Label | null = null;
+
+    @property
+    redBoneName = "toujiaoR";
+    @property
+    greenBoneName = "shouL";
+    @property
+    blueBoneName = "bone24";
+
+
+
+    generateAllNodes () {
+        this.destroyAllNodes();
+        let red = this.createSocket(this.redBoneName, new Color(255, 0, 0))
+        let green = this.createSocket(this.greenBoneName, new Color(0, 255, 0))
+        let blue = this.createSocket(this.blueBoneName, new Color(0, 0, 255))
+        this.skeleton!.sockets = [red, green, blue];
+    }
+
+    destroyUnusual () {
+        this.destroyAllNodes();
+    }
+
+    destroyAllNodes () {
+        let sockets = this.skeleton!.sockets;
+        for(let s of sockets) {
+            s.target!.removeFromParent();
+        }
+        this.skeleton!.sockets = [];
+    }
+
+    generateSomeNodes () {
+        let sockets = this.skeleton!.sockets;
+        let greens = sockets.filter(x => x.target?.name == this.greenBoneName);
+        if(greens.length === 0) {
+            let green = this.createSocket(this.greenBoneName, new Color(0, 255, 0))
+            sockets.push(green);
+            this.skeleton!.sockets = sockets;
+        }
+    }
+
+    destroySomeNodes () {
+        let sockets = this.skeleton!.sockets;
+        for(let l = sockets.length -1; l >= 0; l --) {
+            if(sockets[l].target!.name === this.greenBoneName){
+                let s = sockets.splice(l, 1);
+                s[0].target!.removeFromParent();
+                this.skeleton!.sockets = sockets;
+                break;
+            }
+        }
+    }
+
+    changeMode () {
+        let isCached = this.skeleton!.isAnimationCached();
+        if (isCached) {
+            this.skeleton!.setAnimationCacheMode(dragonBones.ArmatureDisplay.AnimationCacheMode.REALTIME);
+            this.modeLabel!.string = "cache";
+        } else {
+            this.skeleton!.setAnimationCacheMode(dragonBones.ArmatureDisplay.AnimationCacheMode.SHARED_CACHE);
+            this.modeLabel!.string = "realtime";
+        }
+    }
+
+    private createSocket(name:string, color:Color) {
+        let dbnode = new dragonBones.DragonBoneSocket();
+        dbnode.path = this.skeleton!.querySocketPathByName(name)[0];
+        const child = dbnode.target = instantiate(this.targetPrefab!);
+        child.parent = this.node;
+        child.name = name;
+        const sp = child.getComponent(Sprite)!;
+        sp.color = color;
+        return dbnode;
+    }
+}
---
+++ assets/cases/dragonbones/ReplaceSlotDisplay.ts
@@ -0,0 +1,51 @@
+
+
+
+import { _decorator, Component, director, instantiate, Label, Color, Vec2, Node, Prefab, dragonBones, macro, SystemEventType, systemEvent, Vec3, Material } from 'cc';
+import { SystemEvent } from '../event/system-event/system-event';
+const { ccclass, property, requireComponent } = _decorator;
+
+
+@ccclass('ReplaceSlotDisplay')
+export class ReplaceSlotDisplay extends Component {
+
+    @property({ type: dragonBones.ArmatureDisplay })
+    armatureDisplay: dragonBones.ArmatureDisplay | null = null
+    @property({ type: dragonBones.ArmatureDisplay })
+    replaceArmatureDisplay: dragonBones.ArmatureDisplay | null = null;
+
+
+    _leftWeaponIndex = 0;
+    _rightDisplayIndex = 0;
+    _rightDisplayNames:string[] = [];
+    _rightDisplayOffset:{x: number, y: number}[] = [];
+
+    start () {
+        this.replaceArmatureDisplay!.node.active = false;
+        this._leftWeaponIndex = 0;
+        this._rightDisplayIndex = 0;
+        this._rightDisplayNames = ["weapon_1004_r", "weapon_1004d_r"];
+        this._rightDisplayOffset = [{ x: 0, y: 0 }, { x: -60, y: 100 }];
+    }
+
+    left () {
+        let armature = this.armatureDisplay!.armature();
+        let slot = armature.getSlot("weapon_hand_l");
+        slot.displayIndex = slot.displayIndex == 0 ? 4 : 0;
+    }
+
+    right () {
+        this._rightDisplayIndex++;
+        this._rightDisplayIndex %= this._rightDisplayNames.length;
+        let armature = this.armatureDisplay!.armature();
+        let slot = armature.getSlot("weapon_hand_r");
+        const displayName = this._rightDisplayNames[this._rightDisplayIndex];
+        let factory = dragonBones.CCFactory.getInstance() as any;
+        factory.replaceSlotDisplay(this.replaceArmatureDisplay!.getArmatureKey(), "weapon", "weapon_r", displayName, slot);
+
+        let offset = this._rightDisplayOffset[this._rightDisplayIndex];
+        slot.parent.offset.x = offset.x;
+        slot.parent.offset.y = offset.y;
+        armature.invalidUpdate();
+    }
+}
