c6c21bacc59ada28ddea38abf6e3c4231fe61f3d
JoneLau <32630749+JoneLau@users.noreply.github.com>
2020-12-11 19:36:57 +0800

    simultaneously modify test cases according to mask pr (#363)

--- assets/cases/ui/08.mask/mask-use-image-stencil.ts
+++ assets/cases/ui/08.mask/mask-use-image-stencil.ts
@@ -17,35 +17,39 @@ export class MaskUseImageStencil extends Component {
     label: Label = null;
 
     start(){
-        const mask = this.getComponent(Mask);
+        const mask = this.getComponent(Mask)!;
 
         this.scheduleOnce(() => {
             mask.type = Mask.Type.IMAGE_STENCIL;
-            this.scheduleOnce(() => {
-                mask.type = Mask.Type.GRAPHICS_STENCIL;
-                const g = mask.graphics;
-                g.clear();
-                g.lineWidth = 10;
-                g.fillColor.fromHEX('#ff0000');
-
-                g.moveTo(-80, 0);
-                g.lineTo(0, -150);
-                g.lineTo(80, 0);
-                g.lineTo(0, 150);
-                g.close();
-
-                g.stroke();
-                g.fill();
+            this.scheduleOnce(()=>{
+                mask.enabled = false;
                 this.scheduleOnce(() => {
-                    mask.spriteFrame = mask.spriteFrame = this.image;
-                    mask.type = Mask.Type.IMAGE_STENCIL;
-                    mask.alphaThreshold = 0.1;
+                    mask.type = Mask.Type.GRAPHICS_STENCIL;
+                    const g = mask.graphics;
+                    g.clear();
+                    g.lineWidth = 10;
+                    g.fillColor.fromHEX('#ff0000');
+
+                    g.moveTo(-80, 0);
+                    g.lineTo(0, -150);
+                    g.lineTo(80, 0);
+                    g.lineTo(0, 150);
+                    g.close();
+
+                    g.stroke();
+                    g.fill();
+                    mask.enabled = true;
                     this.scheduleOnce(() => {
-                        mask.type = Mask.Type.RECT;
-                        this.label.string = '测试完成';
+                        mask.spriteFrame = mask.spriteFrame = this.image;
+                        mask.type = Mask.Type.IMAGE_STENCIL;
+                        mask.alphaThreshold = 0.1;
+                        this.scheduleOnce(() => {
+                            mask.type = Mask.Type.RECT;
+                            this.label.string = '测试完成';
+                        }, 2);
                     }, 2);
-                }, 2);
-            }, 2);
+                }, 1);
+            }, 1)
         }, 2);
     }
 }
