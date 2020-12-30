36c40812a8fcf65946da7aa98478e4310a406c06
ChiaNing <sun.cloud.mo@gmail.com>
2020-12-30 21:44:16 +0800

    Revert "gamePause case change (#379)"
    This reverts commit 48337299ca67d435e7115ec3a4b40e8247945a25.

--- assets/cases/base/pauseButton.ts
+++ assets/cases/base/pauseButton.ts
@@ -1,13 +1,35 @@
-import { _decorator, Component, director } from "cc";
+import { _decorator, Component, game, Game } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("button")
 export class button extends Component {
 
+    private isPaused = false;
+    start () {
+        // Your initialization goes here.
+        game.on(Game.EVENT_SHOW, this.resumeState, this);
+    }
+
+    onDestroy () {
+        game.off(Game.EVENT_SHOW, this.resumeState, this);
+    }
+
+    resumeState () {
+        if (this.isPaused) {
+            this.onPause();
+        } else {
+            this.onResume();
+        }
+    }
+
     onPause () {
-        director.pause();
+        if(game.isPaused()) return;
+        this.isPaused=true;
+        game.pause();
     }
     onResume () {
-        director.resume();
+        if(!game.isPaused()) return;
+        this.isPaused = false;
+        game.resume();
     }
 }
