c25f8a72ab04c92b994111d972282daef0dac3c1
ChiaNing <sun.cloud.mo@gmail.com>
2020-12-31 17:22:22 +0800

    revert commit update again

--- assets/cases/base/pauseButton.ts
+++ assets/cases/base/pauseButton.ts
@@ -1,35 +1,13 @@
-import { _decorator, Component, game, Game } from "cc";
+import { _decorator, Component, director } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("button")
 export class button extends Component {
 
-    private isPaused = false;
-    start () {
-        // Your initialization goes here.
-        game.on(Game.EVENT_SHOW, this.resumeState, this);
-    }
-
-    onDestroy () {
-        game.off(Game.EVENT_SHOW, this.resumeState, this);
-    }
-
-    resumeState () {
-        if (this.isPaused) {
-            this.onPause();
-        } else {
-            this.onResume();
-        }
-    }
-
     onPause () {
-        if(game.isPaused()) return;
-        this.isPaused=true;
-        game.pause();
+        director.pause();
     }
     onResume () {
-        if(!game.isPaused()) return;
-        this.isPaused = false;
-        game.resume();
+        director.resume();
     }
 }
