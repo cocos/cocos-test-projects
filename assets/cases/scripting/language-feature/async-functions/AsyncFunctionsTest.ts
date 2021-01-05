7aa9addfa24c545e06267eab9e41e456488af747
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2021-01-05 15:34:11 +0800

    Update AsyncFunctionsTest.ts

--- assets/cases/scripting/language-feature/async-functions/AsyncFunctionsTest.ts
+++ assets/cases/scripting/language-feature/async-functions/AsyncFunctionsTest.ts
@@ -41,7 +41,7 @@ export class AsyncFunctionsTest extends cc.Component {
 }
 
 async function sleep (duration: number) { // Define an async function should be OK.
-    return await new Promise((resolve, reject) => { // `await` in async function should be OK.
+    return await new Promise<void>((resolve, reject) => { // `await` in async function should be OK.
         setTimeout(() => {
             resolve();
         }, duration);
@@ -49,7 +49,7 @@ async function sleep (duration: number) { // Define an async function should be 
 }
 
 const sleepThrow = async (at: number) => { // Define an async lambda should be OK.
-    return await new Promise((resolve, reject) => { // `await` in lambda should be OK.
+    return await new Promise<void>((resolve, reject) => { // `await` in lambda should be OK.
         setTimeout(() => {
             reject(new Error(`Oops...`));
         }, at);
