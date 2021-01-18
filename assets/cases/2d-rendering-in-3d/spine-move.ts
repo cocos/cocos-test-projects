c1d89cfb8c27ae73f595d269f60c9e0a1a2ce52e
mmyduckx <308303735@qq.com>
2021-01-18 18:14:37 +0800

    addd spine/dragonbones in 3d scene case

---
+++ assets/cases/2d-rendering-in-3d/spine-move.ts
@@ -0,0 +1,40 @@
+
+import { _decorator, Component, Node, Vec3 } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('SpineMove')
+export class SpineMove extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+
+    start () {
+        // [3]
+        let x = -5;
+        this.schedule((dt:number) => {
+            x += dt;
+            this.node.position = new Vec3(x, 0 , 0);
+            if( x >= 5) {
+                x = -5;
+            }
+        });
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */
