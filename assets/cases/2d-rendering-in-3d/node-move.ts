20ac641cbaa64764da9ca20a43da4ede7491cbbc
mmyduckx <308303735@qq.com>
2021-01-19 14:41:39 +0800

    merge typescripts to node-move v2

--- assets/cases/2d-rendering-in-3d/node-move.ts
+++ assets/cases/2d-rendering-in-3d/node-move.ts
@@ -9,9 +9,11 @@ export class NodeMove extends Component {
         let x = this.node.position.x;
         let y = this.node.position.y;
         let z = this.node.position.z;
+        let vec3 = new Vec3(x, y , z);
         this.schedule((dt:number) => {
             x += dt;
-            this.node.position = new Vec3(x, y , z);
+            vec3.x = x;
+            this.node.setPosition(vec3);
             if( x >= 5) {
                 x = -5;
             }
