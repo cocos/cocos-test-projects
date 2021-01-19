57d20ecfab51a57563ee986b9d23d02e638ca4d9
mmyduckx <308303735@qq.com>
2021-01-19 14:53:46 +0800

    2d rendering in 3d (#395)
    * addd spine/dragonbones in 3d scene case
    
    * add spine/dragbones/tilemap redenring case in 3d
    
    * merge typescripts to node-move
    
    * merge typescripts to node-move v2
    
    * merge typescripts to node-move v2.1

---
+++ assets/cases/2d-rendering-in-3d/node-move.ts
@@ -0,0 +1,23 @@
+
+import { _decorator, Component, Node, Vec3 } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('NodeMove')
+export class NodeMove extends Component {
+
+    start () {
+        let x = this.node.position.x;
+        let y = this.node.position.y;
+        let z = this.node.position.z;
+        let vec3 = new Vec3(x, y , z);
+        this.schedule((dt:number) => {
+            x += dt;
+            vec3.x = x;
+            this.node.setPosition(vec3);
+            if (x >= 5) {
+                x = -5;
+            }
+        });
+    }
+
+}
