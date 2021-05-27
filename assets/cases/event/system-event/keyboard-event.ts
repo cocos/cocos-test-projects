3291158dc22e8d0e0afd76d5d6f64b3fac83a858
PP_Pro <chinese1236@qq.com>
2021-05-27 21:41:52 +0800

    UPDATE
    UPDATE

--- assets/cases/event/system-event/keyboard-event.ts
+++ assets/cases/event/system-event/keyboard-event.ts
@@ -1,29 +1,144 @@
-
-import { _decorator, Component, Node, systemEvent, SystemEventType, EventKeyboard, tween } from 'cc';
+import { _decorator, Component, Node, systemEvent, SystemEventType, EventKeyboard, tween, log, Color, Sprite } from 'cc';
 const { ccclass, property } = _decorator;
+
+const keyCode2KeyName: Record<string, string> = {
+    "0": "none",
+    "6": "back",
+    "8": "backspace",
+    "9": "tab",
+    "13": "enter",
+    "16": "shift",
+    "17": "ctrl",
+    "18": "alt",
+    "19": "pause",
+    "20": "capslock",
+    "27": "escape",
+    "32": "space",
+    "33": "pageup",
+    "34": "pagedown",
+    "35": "end",
+    "36": "home",
+    "37": "left",
+    "38": "up",
+    "39": "right",
+    "40": "down",
+    "41": "select",
+    "45": "insert",
+    "46": "Delete",
+    "48": "0",
+    "49": "1",
+    "50": "2",
+    "51": "3",
+    "52": "4",
+    "53": "5",
+    "54": "6",
+    "55": "7",
+    "56": "8",
+    "57": "9",
+    "65": "a",
+    "66": "b",
+    "67": "c",
+    "68": "d",
+    "69": "e",
+    "70": "f",
+    "71": "g",
+    "72": "h",
+    "73": "i",
+    "74": "j",
+    "75": "k",
+    "76": "l",
+    "77": "m",
+    "78": "n",
+    "79": "o",
+    "80": "p",
+    "81": "q",
+    "82": "r",
+    "83": "s",
+    "84": "t",
+    "85": "u",
+    "86": "v",
+    "87": "w",
+    "88": "x",
+    "89": "y",
+    "90": "z",
+    "96": "num0",
+    "97": "num1",
+    "98": "num2",
+    "99": "num3",
+    "100": "num4",
+    "101": "num5",
+    "102": "num6",
+    "103": "num7",
+    "104": "num8",
+    "105": "num9",
+    "106": "*",
+    "107": "+",
+    "109": "-",
+    "110": "numdel",
+    "111": "/",
+    "112": "f1",
+    "113": "f2",
+    "114": "f3",
+    "115": "f4",
+    "116": "f5",
+    "117": "f6",
+    "118": "f7",
+    "119": "f8",
+    "120": "f9",
+    "121": "f10",
+    "122": "f11",
+    "123": "f12",
+    "144": "numlock",
+    "145": "scrolllock",
+    "186": "semicolon",
+    "187": "=",
+    "188": "comma",
+    "189": "dash",
+    "190": "period",
+    "191": "forwardslash",
+    "192": "grave",
+    "219": "openbracket",
+    "220": "backslash",
+    "221": "closebracket",
+    "222": "quote",
+    "1000": "dpadLeft",
+    "1001": "dpadRight",
+    "1003": "dpadUp",
+    "1004": "dpadDown",
+    "1005": "dpadCenter"
+};
 
 @ccclass('KeyboardEvent')
 export class KeyboardEvent extends Component {
+    private _keyNode2TimeoutId: WeakMap<Node, number | null> = new WeakMap();
+
     onLoad () {
-        systemEvent.on(SystemEventType.KEY_DOWN, this.onKeyboardDown, this);
+        systemEvent.on(SystemEventType.KEYBOARD_DOWN, this.onKeyboardDown, this);
+        systemEvent.on(SystemEventType.KEYBOARD_UP, this.onKyeboardUp, this);
     }
 
     onDestroy () {
         systemEvent.off(SystemEventType.KEYBOARD_DOWN, this.onKeyboardDown, this);
+        systemEvent.off(SystemEventType.KEYBOARD_UP, this.onKyeboardUp, this);
     }
     
     onKeyboardDown (event: EventKeyboard) {
-        console.log('down', event.keyCode);
-        let a = this.getChildRecursively(this.node, event.keyCode.toString());
-        console.log('pptest', a);
+        let keyCode = event.keyCode.toString();
+        let keyName = keyCode2KeyName[keyCode];
+        log('key down: ', keyName);
+        let keyNode = this.getChildRecursively(this.node, keyName);
+        if (keyNode) {
+            this.showKeyNodeWithColor(keyNode, Color.RED);
+        }
+    }
 
-        if (a) {
-            a.active = true;
-            this.scheduleOnce(() => {
-                if (a) {
-                    a.active = false;
-                }
-            }, 0.2);
+    onKyeboardUp (event: EventKeyboard) {
+        let keyCode = event.keyCode.toString();
+        let keyName = keyCode2KeyName[keyCode];
+        log('key up: ', keyName);
+        let keyNode = this.getChildRecursively(this.node, keyName);
+        if (keyNode) {
+            this.showKeyNodeWithColor(keyNode, Color.BLUE);
         }
     }
 
@@ -40,6 +155,20 @@ export class KeyboardEvent extends Component {
             }
         }
         return null;
+    }
+
+    showKeyNodeWithColor (node: Node, color: Color) {
+        let timeoutId = this._keyNode2TimeoutId.get(node);
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        node.getComponent(Sprite)!.color = color;
+        node.active = true;
+        timeoutId = setTimeout(() => {
+            node.active = false;
+        }, 200);
+        this._keyNode2TimeoutId.set(node, timeoutId);
     }
 }
 
