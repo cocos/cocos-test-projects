d18e4da874c8f18ada8de40d08676ba4712fd205
ChiaNing <sun.cloud.mo@gmail.com>
2022-01-26 16:51:39 +0800

    add motion-streak color change button

--- assets/cases/ui/24.motion-streak/motion-streak-ctrl.ts
+++ assets/cases/ui/24.motion-streak/motion-streak-ctrl.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, MotionStreak, Texture2D, Animation } from 'cc';
+import { _decorator, Component, Node, MotionStreak, Texture2D, Animation, Color } from 'cc';
 const { ccclass, type } = _decorator;
 
 @ccclass('MotionStreakCtrl')
@@ -16,9 +16,14 @@ export class MotionStreakCtrl extends Component {
     private _changed: boolean = true;
     private _oldTexture: Texture2D | null = null;
 
+    private _colorChanged: boolean = false;
+    private _newColor = Color.CYAN;
+    private _oldColor = Color.WHITE;
+
     onLoad () {
         this._changed = true;
         this._oldTexture = this.motionStreak!.texture;
+        this._colorChanged = false;
     }
 
     onClick () {
@@ -29,6 +34,15 @@ export class MotionStreakCtrl extends Component {
             this.setMotionStreak(0.5, 1, 30, this._oldTexture!);
         }
         this._changed = !this._changed;
+    }
+
+    colorChange () {
+        this._colorChanged = !this._colorChanged;
+        if (this._colorChanged) {
+            this.motionStreak!.color = this._newColor;
+        } else {
+            this.motionStreak!.color = this._oldColor;
+        }
     }
 
     setMotionStreak (fadeTime: number, minSeg: number, stroke: number, texture: Texture2D) {
