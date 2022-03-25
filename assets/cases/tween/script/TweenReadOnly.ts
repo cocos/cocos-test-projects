43d2cf6b117f3901beb78b9632d9cba3bdbf8ea1
ChiaNing <sun.cloud.mo@gmail.com>
2022-03-25 16:28:47 +0800

    merge 3.4 to 3.5 (#593)
    * Fix ui blend issue on web (#554)
    
    * Fix canvas background in canvasUseRenderTex (#555)
    
    * Fix ui blend issue on web
    
    * Fix canvas background in canvasUseRenderTex
    
    * Revert "Fix ui blend issue on web (#554)" (#556)
    
    This reverts commit 94ad4c5b4716bbff55ed2e75670809b038eade4f.
    
    * fix issue10203, change loadtips initialization when curtype===lasttype (#558)
    
    * ignore mouse move event after mouse up (#552)
    
    * fix mouse move
    
    * update containerStrategy test case
    
    * update
    
    * update
    
    * update
    
    * 修复地形lod不生效的问题 (#557)
    
    * Revert "Revert "Fix ui blend issue on web (#554)" (#556)" (#559)
    
    This reverts commit f81c3483f5389c1c1cc04260e65aeb88e8a90c22.
    
    * update camera clear flag (#561)
    
    * add labelspacingx testcase (#562)
    
    * add labelspacingx testcase
    
    * add auto test scene
    
    * update indent
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    
    * add tween stop all by tag test-case (#564)
    
    * Default texture unit test@xianjie.cai (#565)
    
    * add builtin texture test case
    
    * modify skybox luminance
    
    * add builtin texture test
    
    * add richtext truncation testcase (#563)
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    
    * Revert "add tween stop all by tag test-case (#564)" (#567)
    
    This reverts commit 0b22e8c1d555e28388c09221ee81b11edc3d29ab.
    
    * update camera clear flag (#568)
    
    * Revert "add richtext truncation testcase (#563)" (#569)
    
    This reverts commit ccda136d551600dcc2812d72607188a40d1044a2.
    
    * optimize input test-case (#570)
    
    * update pointer swallow test
    
    * support event order test case
    
    * fix off event
    
    * add tween-readonly and tween-repeat-union test cases (#571)
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    
    * add motion-streak color change button (#573)
    
    * optimize label-spacing-x test case (#572)
    
    (cherry picked from commit 2768a4248f988c02b65273f731f470b13dacd4e5)
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    
    * Using builtin physics 2d (#576)
    
    * add fuzzyText test case (#577)
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    
    * Add compress texture with gray case (#579)
    
    * Add compress texture with gray case
    
    * auto test case add
    
    * remove useless flags in engine settings (#582)
    
    * add tween node null condition (#581)
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    
    * revert fuzzytext test case (#586)
    
    Co-authored-by: pandamicro <lphuabin@gmail.com>
    Co-authored-by: Greg1129 <95199410+Greg1129@users.noreply.github.com>
    Co-authored-by: PP <chinese1236@qq.com>
    Co-authored-by: Myway <MSoft1115@hotmail.com>
    Co-authored-by: Yun Hsiao Wu <yunhsiaow@gmail.com>
    Co-authored-by: Zhang mingzhen <32831993+Zhmz@users.noreply.github.com>
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>
    Co-authored-by: Santy-Wang <wh19930829@qq.com>

---
+++ assets/cases/tween/script/TweenReadOnly.ts
@@ -0,0 +1,79 @@
+
+import { _decorator, Component, Node, Sprite, Vec3, tween } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = TweenReadOnly
+ * DateTime = Wed Jan 19 2022 16:38:23 GMT+0800 (中国标准时间)
+ * Author = zmzczy
+ * FileBasename = TweenReadOnly.ts
+ * FileBasenameNoExtension = TweenReadOnly
+ * URL = db://assets/cases/tween/script/TweenReadOnly.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
+ *
+ */
+ 
+@ccclass('TweenReadOnly')
+export class TweenReadOnly extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+
+
+    spriteRed: Sprite = null!;
+    spriteGreen: Sprite = null!;
+
+    oriRedPos:Vec3 = null!;
+    oriGreenPos:Vec3 = null!;
+
+    start () {
+        // [3]
+        this.spriteRed = this.node.getChildByName('SpriteRed')?.getComponent<Sprite>(Sprite)!;
+        this.oriRedPos = this.spriteRed.node.position;
+        this.spriteGreen = this.node.getChildByName('SpriteGreen')?.getComponent<Sprite>(Sprite)!;
+        this.oriGreenPos = this.spriteGreen.node.position;
+
+        this.tweenStart();
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+
+
+    tweenStart() {
+        tween(this.spriteRed.node)
+            .to(2,{position:new Vec3(this.oriRedPos.x,-200,0)})
+            .call(()=> {
+                if(this.spriteRed && this.spriteRed.node) {
+                    this.spriteRed.node.position = new Vec3(this.oriRedPos.x,0,0)
+            }})
+            .union()
+            .repeatForever()
+            .start();
+        tween(this.spriteGreen.node.position)
+            .to(2,{y:-200})
+            .call(()=>{
+                if(this.spriteGreen && this.spriteGreen.node) {
+                    this.spriteGreen.node.position = new Vec3(this.oriGreenPos.x,0,0)
+            }})
+            .union()
+            .repeatForever()
+            .start();
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/en/scripting/decorator.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/en/scripting/life-cycle-callbacks.html
+ */
