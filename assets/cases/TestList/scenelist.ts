6d5dc4a86b5547ff33d8a6978405de121c7ad0ad
liuyi <liuyi@cocos.com>
2022-04-21 15:43:45 +0800

    修改测试用例场景结构

--- assets/cases/TestList/scenelist.ts
+++ assets/cases/TestList/scenelist.ts
@@ -1,19 +1,41 @@
 import { _decorator, Component, Node, Prefab, instantiate } from "cc";
 const { ccclass, property } = _decorator;
 
-export const sceneArray:string[] = [];
+export class SceneList {
+
+    static sceneArray : string[] = [];
+    static sceneFold : string[] = [];
+    static foldCount : number = 0;
+
+}
 
 @ccclass("scenemanager")
 export class SceneManager extends Component {
 
     @property ({ type: Prefab })
     itemPrefab: Prefab | null  = null;
+    @property ({type: Prefab})
+    foldPrefab: Prefab | null = null;
 
     onLoad() {
-        if(this.itemPrefab){
-            for(let i = 0; i<sceneArray.length; i++ ) {
+        SceneList.foldCount = 0;
+        if(this.itemPrefab && this.foldPrefab){     
+            // instantiate first item
+            let itemFold = instantiate(this.foldPrefab);
+            this.node.addChild(itemFold);
+            let isChange = false;              
+            for(let i = 0; i< SceneList.sceneArray.length; i++ ) {   
                 let item = instantiate(this.itemPrefab);
-                this.node.addChild(item);
+                this.node.addChild(item);         
+                // 判断是否需要添加模块名
+                if(i + 1 < SceneList.sceneFold.length && SceneList.sceneFold[i] !== SceneList.sceneFold[i + 1]) {
+                    isChange = true;
+                }
+                if (isChange) { // 加模块名
+                    let itemFold = instantiate(this.foldPrefab);
+                    this.node.addChild(itemFold);
+                    isChange = false;
+                }             
             }
         }
     }
--- assets/cases/physics/README.md
+++
@@ -1,3 +0,0 @@
-# Physics test case
-
-This test item has been moved [here](https://github.com/cocos-creator/example-3d/tree/master/physics-3d).
