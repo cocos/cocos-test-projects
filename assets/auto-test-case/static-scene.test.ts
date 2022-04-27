0776d3d6c7eba2a3b1cc7b1165448438260716fa
timlyeee <timelyeee@gmail.com>
2022-04-27 10:17:30 +0800

    Auto test for static scene only
    PR by He Xiaolong

---
+++ assets/auto-test-case/static-scene.test.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import { captureOneImage, loadScene, runScene, sleep, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { delayTime, sceneNameList } from '../static-scene-list';
+
+@runScene('atlas-compress')
+@testClass('StaticScene')
+export class StaticScene {
+    _delay = delayTime;
+    @testCase
+    async switchScene() {
+        console.log('场景列表:  ', sceneNameList);
+        console.log('delayTime: ', delayTime);
+        for (let i = 0; i < sceneNameList.length; i++) {
+            await sleep(this._delay);
+            await loadScene(sceneNameList[i])?.catch((e: any) => { console.log(e) });
+            // 截图 or 断言
+            await captureOneImage(sceneNameList[i]);
+        };
+    };
+}
