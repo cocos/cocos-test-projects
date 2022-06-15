5dc005635f903a4a4e3a58ed50266de217993045
LIUYIa <102024185+LIUYIa@users.noreply.github.com>
2022-06-15 22:12:03 +0800

    Ignore some scenes on NX platform (#638)
    Co-authored-by: liuyi <liuyi@cocos.com>

--- assets/cases/geometry-renderer/geometryRenderer.ts
+++ assets/cases/geometry-renderer/geometryRenderer.ts
@@ -1,8 +1,12 @@
 import { _decorator, Component, Node, Toggle, sys } from 'cc';
+import { GeometryCreator } from './geometry-creator';
 const { ccclass, property } = _decorator;
 
 @ccclass('geometryRenderer')
 export class geometryRenderer extends Component {
+
+    @property(GeometryCreator)
+    creator: GeometryCreator = null!;
 
     @property(Node)
     public noSupport: Node = null!
@@ -10,9 +14,11 @@ export class geometryRenderer extends Component {
     onLoad () {
         if (sys.platform === sys.Platform.NX ) {
             this.noSupport.active = true;
+            this.creator.enabled = false;
             return;
         } else {
-            this.noSupport.active = false;         
+            this.noSupport.active = false;  
+            this.creator.enabled = true;       
         }
     }
 }
