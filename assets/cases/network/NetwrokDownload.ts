64f08992a0564a52f9d3f772e02f3a6ee651ef3c
mmyduckx <308303735@qq.com>
2022-06-18 15:25:51 +0800

    add network-download testcase

---
+++ assets/cases/network/NetwrokDownload.ts
@@ -0,0 +1,54 @@
+
+import { _decorator, Component, Node, Label, sys } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('test')
+export class test extends Component {
+
+    @property(Label)
+    text: Label = null!;
+
+    @property(Label)
+    status: Label = null!;
+
+    @property(Node)
+    public noSupport: Node = null!
+
+    onLoad () {
+        if (sys.isNative && (sys.platform === sys.Platform.WIN32 ||
+            sys.platform === sys.Platform.MACOS ||
+            sys.platform === sys.Platform.ANDROID ||
+            sys.platform === sys.Platform.IOS)) {
+            this.noSupport.active = false;
+        } else {
+            this.noSupport.active = true;
+            return;
+        }
+    }
+
+    start() {
+        this.status.string = "status: Not start"
+    }
+
+    down () {
+        let down = new jsb.Downloader();
+        down.setOnTaskProgress((task, bytesReceived, totalBytesReceived, totalBytesExpected)=>{
+            console.log(bytesReceived, totalBytesReceived, totalBytesExpected)
+            let progress = (totalBytesReceived / totalBytesExpected * 100).toFixed(1);
+            this.text.string = totalBytesReceived + " ==> " + progress + "%";
+        });
+        down.setOnFileTaskSuccess((task)=>{
+            console.log("Download success!");
+            this.status.string = "status: Success"
+        });
+        down.setOnTaskError((task, errorCode, errorCodeInternal, errorStr)=>{
+            console.log("Task error:", errorStr);
+            this.status.string = "status: Error"
+        });
+        let task = down.createDownloadFileTask('https://all.res-gamebox.cocos.com/res/Cocos_Game_Tools_v1.0.15.apk', jsb.fileUtils.getWritablePath() + 'zz');
+        // let task = down.createDownloadFileTask('https://download.cocos.com/CocosCreator/v2.3.1/CocosCreator_v2.3.1_20200303_win.7z', jsb.fileUtils.getWritablePath() + 'zz');
+        this.status.string = "status: Downloading"
+        
+    }
+
+}
