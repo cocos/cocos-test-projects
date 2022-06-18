16cbab91deb1ca59691f154715f857cf0148463e
mmyduckx <308303735@qq.com>
2022-06-18 18:45:41 +0800

    fix network-Download typo and refine (#642)

--- assets/cases/network/NetworkDownload.ts
+++ assets/cases/network/NetworkDownload.ts
@@ -2,8 +2,8 @@
 import { _decorator, Component, Node, Label, sys } from 'cc';
 const { ccclass, property } = _decorator;
 
-@ccclass('test')
-export class test extends Component {
+@ccclass('NetworkDownload')
+export class NetworkDownload extends Component {
 
     @property(Label)
     text: Label = null!;
@@ -30,22 +30,22 @@ export class test extends Component {
         this.status.string = "status: Not start"
     }
 
-    down () {
-        let down = new jsb.Downloader();
-        down.setOnTaskProgress((task, bytesReceived, totalBytesReceived, totalBytesExpected)=>{
+    download () {
+        let downloader = new jsb.Downloader();
+        downloader.setOnTaskProgress((task, bytesReceived, totalBytesReceived, totalBytesExpected)=>{
             console.log(bytesReceived, totalBytesReceived, totalBytesExpected)
             let progress = (totalBytesReceived / totalBytesExpected * 100).toFixed(1);
             this.text.string = totalBytesReceived + " ==> " + progress + "%";
         });
-        down.setOnFileTaskSuccess((task)=>{
+        downloader.setOnFileTaskSuccess((task)=>{
             console.log("Download success!");
             this.status.string = "status: Success"
         });
-        down.setOnTaskError((task, errorCode, errorCodeInternal, errorStr)=>{
+        downloader.setOnTaskError((task, errorCode, errorCodeInternal, errorStr)=>{
             console.log("Task error:", errorStr);
             this.status.string = "status: Error"
         });
-        let task = down.createDownloadFileTask('https://all.res-gamebox.cocos.com/res/Cocos_Game_Tools_v1.0.15.apk', jsb.fileUtils.getWritablePath() + 'zz');
+        let task = downloader.createDownloadFileTask('https://all.res-gamebox.cocos.com/res/Cocos_Game_Tools_v1.0.15.apk', jsb.fileUtils.getWritablePath() + 'zz');
         // let task = down.createDownloadFileTask('https://download.cocos.com/CocosCreator/v2.3.1/CocosCreator_v2.3.1_20200303_win.7z', jsb.fileUtils.getWritablePath() + 'zz');
         this.status.string = "status: Downloading"
         
