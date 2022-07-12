e2bd7d342aa160f20be8e905b1c4b5cb715ef742
James Chen <dumganhar@gmail.com>
2022-07-12 16:17:06 +0800

    Fix wrong result of TestBufferAsset , use a workaround function arrayBufferToHexString instead.

--- assets/cases/asset/test-bufferasset/TestBufferAsset.ts
+++ assets/cases/asset/test-bufferasset/TestBufferAsset.ts
@@ -1,6 +1,24 @@
 import { _decorator, Component, Node, BufferAsset, Label } from 'cc';
 const { ccclass, property } = _decorator;
 
+const asciiCodes = new Uint8Array(
+    Array.prototype.map.call(
+        '0123456789abcdef',
+        char => char.charCodeAt()
+    ) as number[]
+);
+
+function arrayBufferToHexString (arrayBuffer: ArrayBuffer) : string {
+    const buff = new Uint8Array(arrayBuffer);
+    const charCodes = new Uint8Array(buff.length * 2);
+
+    for (let i = 0; i < buff.length; ++i) {
+        charCodes[i * 2] = asciiCodes[buff[i] >>> 4];
+        charCodes[i * 2 + 1] = asciiCodes[buff[i] & 0xf];
+    }
+
+    return String.fromCharCode(...charCodes);
+}
 @ccclass('TestBufferAsset')
 export class TestBufferAsset extends Component {
     @property(BufferAsset)
@@ -9,18 +27,12 @@ export class TestBufferAsset extends Component {
     @property(Node)
     public promptNode: Node | null = null;
 
-    buf2hex (buffer: ArrayBuffer) : string {
-        return [...new Uint8Array(buffer)]
-            .map(x => x.toString(16).padStart(2, '0'))
-            .join('');
-    }
-
     start () {
         const labelComp = this.promptNode?.getComponent<Label>(Label);
         const buffer = this.bufferAsset?.buffer();
         if (buffer && buffer.byteLength > 0) {
             if (labelComp) {
-                const hexStr = this.buf2hex(buffer);
+                const hexStr = arrayBufferToHexString(buffer);
                 console.log('TestBufferAsset scene, BufferAsset is: ' + hexStr);
                 labelComp.string = '加载成功，内容为：' + hexStr;
             }
