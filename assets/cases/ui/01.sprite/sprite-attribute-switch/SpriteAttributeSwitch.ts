9d36c3eb48f66719ed54f2c4ad0ad300e64f37a3
zhangmingzhen <mingzhen.zhang@cocos.com>
2022-08-04 10:13:54 +0800

    sprite switch test case

---
+++ assets/cases/ui/01.sprite/sprite-attribute-switch/SpriteAttributeSwitch.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('SpriteAttributeSwitch')
+export class SpriteAttributeSwitch extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
