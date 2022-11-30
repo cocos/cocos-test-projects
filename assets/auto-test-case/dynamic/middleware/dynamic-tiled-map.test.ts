14fcf179a63b655d85e7c57f3faa366edfb76cf6
linzihuang <zihuang.lin@chukong-inc.com>
2022-11-30 15:48:59 +0800

    完成dynamic-tiled-map测试例修改

--- assets/auto-test-case/dynamic/middleware/dynamic-tiled-map.test.ts
+++ assets/auto-test-case/dynamic/middleware/dynamic-tiled-map.test.ts
@@ -1,14 +1,25 @@
-import { find } from 'cc';
+import { find, Button } from 'cc';
 // @ts-ignore
 import { captureOneImage, runScene, sleep, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 
 @runScene('dynamic-tiled-map')
-// @testClass('DynamicTiledMap')
+@testClass('DynamicTiledMap')
 export class DynamicTiledMap {
     _dt = 2;
     _delay = 0.5;
 
+    @testCase
+    async startPlay() {
+        await screenshot_custom();
+        find('Canvas/Button')!.getComponent(Button)?.clickEvents[0].emit([]);
+        await sleep(this._delay);
+        await screenshot_custom();
+        find('Canvas/Button-001')!.getComponent(Button)?.clickEvents[0].emit([]);
+        await sleep(this._delay);
+        await screenshot_custom();
+    }
+    /**
     @testCase
     async onBtnCreateTileMap() {
         // @ts-ignore
@@ -24,5 +35,6 @@ export class DynamicTiledMap {
         await sleep(this._delay);
         await screenshot_custom(this._dt);
     }
+     */
 
 }
