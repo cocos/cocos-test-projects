98e23b16f8d4b8dd3692a6b1d21181094c26cc2d
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-01 18:00:23 +0800

    完成particle-trail1测试例。截6张图，一个周期

--- assets/auto-test-case/dynamic/particle/particle-trail1.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-trail1.test.ts
@@ -3,13 +3,14 @@ import { runScene, testCase, testClass } from 'db://automation-framework/runtime
 import { screenshot_custom } from '../common/utils';
 
 @runScene('particle-trail1')
-// @testClass('ParticleTrail1')
+@testClass('ParticleTrail1')
 export class ParticleTrail1 {
     _delay = 0.2;
-    _dt = 30;
+    _dt = 60;
 
     @testCase
     async startPlay() {
+        await screenshot_custom();
         for (let i = 0; i < 5; i++) {
             await screenshot_custom(this._dt);
         };
