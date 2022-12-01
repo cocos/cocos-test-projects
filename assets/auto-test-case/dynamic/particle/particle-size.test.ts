250616bdc07265a86976718152cf153d1383111e
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-01 17:49:30 +0800

    完成particel-size、particle-texture-animation测试例

--- assets/auto-test-case/dynamic/particle/particle-size.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-size.test.ts
@@ -1,16 +1,27 @@
+import { find } from 'cc';
 // @ts-ignore
 import { captureOneImage, runScene, sleep, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 
 @runScene('particle-size')
-// @testClass('ParticleSize')
+@testClass('ParticleSize')
 export class ParticleSize {
     _dt = 6;
     @testCase
     async startPlay() {
+        await screenshot_custom();
+        await screenshot_custom(this._dt*20);
+
+        // 移动后再截图
+        //@ts-ignore
+        find('Camera').getComponent('first-person-camera')._euler = {x: -10.600000000000007, y: 5.20000000000001, z: 0}
+        await screenshot_custom();
+        await screenshot_custom(this._dt*10);
+        /**
         for (let i = 0; i < 5; i++) {
             await screenshot_custom(this._dt);
         };
+         */
     }
 
 }
--- assets/auto-test-case/dynamic/particle/particle-texture-animation.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-texture-animation.test.ts
@@ -3,14 +3,15 @@ import { runScene, testCase, testClass } from 'db://automation-framework/runtime
 import { screenshot_custom } from '../common/utils';
 
 @runScene('particle-texture-animation')
-// @testClass('ParticleTextureAnimation')
+@testClass('ParticleTextureAnimation')
 export class ParticleTextureAnimation {
     _delay = 0.2;
     _dt = 30;
 
     @testCase
     async startPlay() {
-        for (let i = 0; i < 5; i++) {
+        await screenshot_custom();
+        for (let i = 0; i < 4; i++) {
             await screenshot_custom(this._dt);
         };
     }
