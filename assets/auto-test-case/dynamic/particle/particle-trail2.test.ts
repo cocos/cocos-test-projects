a66c15e303844d7328378785a1d3a63ad06763eb
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-02 10:44:07 +0800

    完成particle-trail2一个周期

--- assets/auto-test-case/dynamic/particle/particle-trail2.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-trail2.test.ts
@@ -3,13 +3,13 @@ import { runScene, testCase, testClass } from 'db://automation-framework/runtime
 import { screenshot_custom } from '../common/utils';
 
 @runScene('particle-trail2')
-// @testClass('ParticleTrail2')
+@testClass('ParticleTrail2')
 export class ParticleTrail2 {
-    _delay = 0.2;
-    _dt = 30;
+    _dt = 60;
 
     @testCase
     async startPlay() {
+        await screenshot_custom();
         for (let i = 0; i < 5; i++) {
             await screenshot_custom(this._dt);
         };
