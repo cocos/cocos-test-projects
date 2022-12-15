8c78d389fdba8733cf291afd2f3f834469fcec1f
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-15 15:54:34 +0800

    修复动态图原生环境下，截图比对不准的bug

--- assets/auto-test-case/dynamic/common/utils.ts
+++ assets/auto-test-case/dynamic/common/utils.ts
@@ -1,7 +1,7 @@
 
 import { director, physics } from 'cc';
 // @ts-ignore
-import { captureOneImage } from 'db://automation-framework/runtime/test-framework.mjs';
+import { captureOneImage, waitForNextFrame } from 'db://automation-framework/runtime/test-framework.mjs';
 
 export async function screenshot_custom(dt?: number, imageName?: string) {
     const frame_time = physics.PhysicsSystem.instance.fixedTimeStep;
@@ -11,6 +11,24 @@ export async function screenshot_custom(dt?: number, imageName?: string) {
         }
     } else {
         director.tick(frame_time);
+    }
+    //director.tick(frame_time);
+    director.pause();
+    await captureOneImage(imageName);
+    director.resume();
+}
+
+// by add lzh：等待帧截图
+export async function screenshot_custom_by_wait(dt?: number, imageName?: string) {
+    const frame_time = physics.PhysicsSystem.instance.fixedTimeStep;
+    if (dt) {
+        for (let i = 0; i < dt; i++) {
+            await waitForNextFrame();
+            //director.tick(physics.PhysicsSystem.instance.fixedTimeStep);
+        }
+    } else {
+        //director.tick(frame_time);
+        await waitForNextFrame();
     }
     //director.tick(frame_time);
     director.pause();
