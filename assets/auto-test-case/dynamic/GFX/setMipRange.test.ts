60a7d90e35b8a1a55ae396165573d436710095ae
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-19 17:46:54 +0800

    完成setMipRange标本编写，放大放大再缩小

---
+++ assets/auto-test-case/dynamic/GFX/setMipRange.test.ts
@@ -0,0 +1,20 @@
+import { director } from 'cc';
+// @ts-ignore
+import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { screenshot_custom_by_wait } from '../common/utils';
+
+@runScene('setMipRange')
+@testClass('setMipRange')
+export class setMipRange{
+    _dt = 50;
+    _totalFrames = 0;
+
+    @testCase
+    async startPlay() {
+        await screenshot_custom_by_wait();
+        this._totalFrames = director.getTotalFrames();
+        for (let i = 0; i < 3; i++) {
+            await screenshot_custom_by_wait(this._dt*(i+1)+this._totalFrames-director.getTotalFrames());
+        }
+    }
+}
