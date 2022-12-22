3a3f48592c30cb3c6991ab36d44ccd50b58e23f2
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-22 14:48:29 +0800

    按要求修复ReplaceSlotDisplay

--- assets/auto-test-case/dynamic/middleware/ReplaceSlotDisplay.test.ts
+++ assets/auto-test-case/dynamic/middleware/ReplaceSlotDisplay.test.ts
@@ -1,13 +1,35 @@
 import { find } from 'cc';
 // @ts-ignore
 import { captureOneImage, runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
-import { screenshot_custom } from '../common/utils';
+import { screenshot_custom_by_wait } from '../common/utils';
 
 @runScene('ReplaceSlotDisplay')
 @testClass('ReplaceSlotDisplay')
 export class ReplaceSlotDisplay {
-    _dt = 10;
+    _dt = 20;
 
+    @testCase
+    async startPlay(){
+        await screenshot_custom_by_wait(this._dt);
+        if (find('Canvas')) {
+            // @ts-ignore
+            find('Canvas')!.getComponent('ReplaceSlotDisplay')!.left();
+            await screenshot_custom_by_wait(this._dt);
+            // @ts-ignore
+            find('Canvas')!.getComponent('ReplaceSlotDisplay')!.right();
+            await screenshot_custom_by_wait(this._dt);
+            // @ts-ignore
+            find('Canvas')!.getComponent('ReplaceSlotDisplay')!.right();
+            await screenshot_custom_by_wait(this._dt);
+            // @ts-ignore
+            find('Canvas')!.getComponent('ReplaceSlotDisplay')!.left();
+            await screenshot_custom_by_wait(this._dt);
+        } else {
+            console.error('【TestCaseScript】 ReplaceSlotDisplay scene is error,can not find Canvas Node')
+        }
+    }
+
+    /**
     @testCase
     async start() {
         await screenshot_custom(this._dt);
@@ -41,4 +63,5 @@ export class ReplaceSlotDisplay {
             console.error(`【TestCaseScript】 ReplaceSlotDisplay scene is error:${error}`)
         }
     }
+     */
 }
