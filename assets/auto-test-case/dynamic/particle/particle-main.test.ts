e9a618c16582a29d9233838aa85d815926cb1d90
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-22 18:03:04 +0800

    修复particle-main

--- assets/auto-test-case/dynamic/particle/particle-main.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-main.test.ts
@@ -1,7 +1,7 @@
-import { find, Button } from 'cc';
+import { find, Button, Vec3 } from 'cc';
 // @ts-ignore
 import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
-import { screenshot_custom } from '../common/utils';
+import { screenshot_custom_by_wait } from '../common/utils';
 
 @runScene('particle-main')
 @testClass('ParticleMain')
@@ -12,27 +12,41 @@ export class ParticleMain {
     @testCase
     async startPlay() {
         // begin start to screenshot
-        await screenshot_custom();
+        await screenshot_custom_by_wait();
         // take a screenshot after 30 frames, when listing a particle
-        await screenshot_custom(this._dt*2);
+        await screenshot_custom_by_wait(this._dt*2);
         // take a screenshot at 90 frames, 4 particles in a single row
-        await screenshot_custom(this._dt*6);
+        await screenshot_custom_by_wait(this._dt*6);
         // take a screenshot at 90 frames, 5 grains in a single row
-        await screenshot_custom(this._dt*2);
+        await screenshot_custom_by_wait(this._dt*2);
 
         // button click
         find('Canvas//Button')!.getComponent(Button)?.clickEvents[0].emit([]);
-        await screenshot_custom();
+        await screenshot_custom_by_wait();
 
         // move after the text appears
         //@ts-ignore
-        find('Camera').getComponent('first-person-camera')._euler = {x: -8.600000000000007, y: -2.20000000000001, z: 0}
-        await screenshot_custom(this._dt);
-        /**
-        for (let i = 0; i < 8; i++) {
-            await screenshot_custom(this._dt);
-        };
-         */
+        find('Camera').getComponent('first-person-camera')._euler = {x: 19.1, y: 2.0, z: 0}
+        await screenshot_custom_by_wait(this._dt);
+
+        await this.onMouseWheel(-10);
+        await screenshot_custom_by_wait(this._dt);
+
+        await this.onMouseWheel(30);
+        await screenshot_custom_by_wait(this._dt);
+    }
+
+    // zoom
+    public onMouseWheel (delta=1) {
+        return new Promise((resolve, reject)=>{
+        let _self = find('Camera')!.getComponent('first-person-camera')!;
+        const v3_1 = new Vec3();
+        Vec3.transformQuat(v3_1, Vec3.UNIT_Z, _self.node.rotation);
+        // @ts-ignore
+        Vec3.scaleAndAdd(_self._position, _self.node.position, v3_1, delta);
+        resolve("ok")
+        });
+    
     }
 
 }
