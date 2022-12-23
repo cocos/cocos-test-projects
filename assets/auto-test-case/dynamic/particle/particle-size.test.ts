578020ed2d0e4c758560909ac75abce7809b767b
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-23 13:32:39 +0800

    修复particle-size

--- assets/auto-test-case/dynamic/particle/particle-size.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-size.test.ts
@@ -9,14 +9,14 @@ export class ParticleSize {
     _dt = 6;
     @testCase
     async startPlay() {
-        await screenshot_custom();
-        await screenshot_custom(this._dt*20);
+        await screenshot_custom(this._dt);
+        await screenshot_custom(this._dt*15);
+        await screenshot_custom(this._dt*30);
 
         // screenshot after moving
         //@ts-ignore
         find('Camera').getComponent('first-person-camera')._euler = {x: -10.600000000000007, y: 5.20000000000001, z: 0}
-        await screenshot_custom();
-        await screenshot_custom(this._dt*10);
+        await screenshot_custom(this._dt*5);
         /**
         for (let i = 0; i < 5; i++) {
             await screenshot_custom(this._dt);
