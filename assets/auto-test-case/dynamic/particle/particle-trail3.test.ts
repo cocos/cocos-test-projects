18e2c217a238c81bc80085a36af6c6fc2af08f60
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-23 18:41:44 +0800

    修复particle-trail3

--- assets/auto-test-case/dynamic/particle/particle-trail3.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-trail3.test.ts
@@ -1,17 +1,16 @@
 // @ts-ignore
 import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
-import { screenshot_custom } from '../common/utils';
+import { screenshot_custom_by_wait } from '../common/utils';
 
 @runScene('particle-trail3')
 @testClass('ParticleTrail3')
 export class ParticleTrail3 {
-    _dt = 15;
+    _dt = 18;
 
     @testCase
     async startPlay() {
-        await screenshot_custom();
         for (let i = 0; i < 4; i++) {
-            await screenshot_custom(this._dt);
+            await screenshot_custom_by_wait(this._dt);
         };
     }
 }
