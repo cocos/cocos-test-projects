93200a52b4fa53fba75077fa2627aacc04b6a88d
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-23 14:04:00 +0800

    修复particle-texture-animation和particle-trail1

--- assets/auto-test-case/dynamic/particle/particle-texture-animation.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-texture-animation.test.ts
@@ -1,18 +1,17 @@
 // @ts-ignore
 import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
-import { screenshot_custom } from '../common/utils';
+import { screenshot_custom_by_wait } from '../common/utils';
 
 @runScene('particle-texture-animation')
 @testClass('ParticleTextureAnimation')
 export class ParticleTextureAnimation {
     _delay = 0.2;
-    _dt = 30;
+    _dt = 23;
 
     @testCase
     async startPlay() {
-        await screenshot_custom();
-        for (let i = 0; i < 4; i++) {
-            await screenshot_custom(this._dt);
+        for (let i = 0; i < 5; i++) {
+            await screenshot_custom_by_wait(this._dt);
         };
     }
 }
