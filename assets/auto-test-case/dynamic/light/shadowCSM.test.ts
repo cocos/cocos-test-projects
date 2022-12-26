9da90d1259e6ff7a251a5956f40e66d0d5e5866d
linzihuang <zihuang.lin@chukong-inc.com>
2022-12-26 18:05:56 +0800

    新增shadowCSM用例

---
+++ assets/auto-test-case/dynamic/light/shadowCSM.test.ts
@@ -0,0 +1,38 @@
+import { find, Vec3 } from 'cc';
+// @ts-ignore
+import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { screenshot_custom_by_wait } from '../common/utils';
+
+@runScene('shadowCSM')
+@testClass('ShadowCSM')
+export class ShadowCSM {
+  _dt = 19;
+
+  @testCase
+  async startPlay() {
+    await screenshot_custom_by_wait(this._dt);
+    
+    await this.onMouseWheel(20);
+    await screenshot_custom_by_wait(this._dt);
+    
+    await this.onMouseWheel(-22);
+    await screenshot_custom_by_wait(this._dt);
+    //@ts-ignore
+    find('Main Camera').getComponent('first-person-camera')._euler = {x:18, y: -20, z: 0};
+    await screenshot_custom_by_wait(this._dt);
+  }
+
+  
+  // zoom
+  public onMouseWheel (delta=1) {
+    //const delta = -e.getScrollY() * this.moveSpeed * 0.01; // delta is positive when scroll down
+    return new Promise((resolve, reject)=>{
+      let _self = find('Main Camera')!.getComponent('first-person-camera')!;
+      const v3_1 = new Vec3();
+      Vec3.transformQuat(v3_1, Vec3.UNIT_Z, _self.node.rotation);
+      Vec3.scaleAndAdd(_self._position, _self.node.position, v3_1, delta);
+      resolve("ok")
+    });
+    
+  }
+}
