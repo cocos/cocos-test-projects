bf9d059d5afe492cc652a7c76057d2b2392c84ae
胡志锋 <zhifeng.hu@cocos.com>
2023-02-16 18:32:45 +0800

    解决 DynamicMesh 截取不到龙

--- assets/auto-test-case/dynamic/dynamic-batch/dynamic-mesh.test.ts
+++ assets/auto-test-case/dynamic/dynamic-batch/dynamic-mesh.test.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import { find } from "cc";
 // @ts-ignore
-import { runScene, testCase, testClass,sleep } from "db://automation-framework/runtime/test-framework.mjs";
+import { runScene, testCase, testClass,sleep, waitForFrames } from "db://automation-framework/runtime/test-framework.mjs";
 import { screenshot_custom_by_wait } from "../common/utils";
 
 @runScene("dynamic-mesh")
@@ -11,8 +11,9 @@ export class DynamicMesh {
     // this test scene can't capture,so this test case cancle test
     @testCase
     async start() {
-        //await sleep(2);
-        await screenshot_custom_by_wait(this._dt);
+        waitForFrames(1);
+        await sleep(2);
+        await screenshot_custom_by_wait(this._dt - 1);
     }
 
     @testCase
@@ -21,7 +22,7 @@ export class DynamicMesh {
        find("Canvas").getComponent("DynamicMeshCreator").onButtonUpdateDynamicMesh(find("Canvas/UpdateDynamicMesh")?.getComponent("cc.Button"));
        //await sleep(3)
        for(let i = 0; i < 4; i++){
-            //await sleep(1)
+            await sleep(1)
             await screenshot_custom_by_wait(this._dt*5);
        }
     }
