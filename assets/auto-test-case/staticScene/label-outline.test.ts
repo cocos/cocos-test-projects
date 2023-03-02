c57ad6673a7eefc332da688a9bf9e2018dc1b741
练素琼 <suqiong.lian@cocos.com>
2023-03-02 15:22:59 +0800

    reupload meta file

--- assets/auto-test-case/staticScene/label-outline.test.ts
+++ assets/auto-test-case/staticScene/label-outline.test.ts
@@ -1,12 +1,12 @@
 // @ts-ignore
-import { captureOneImage, waitForNextFrame, runScene, testCase, testClass, PlatformEnum } from 'db://automation-framework/runtime/test-framework.mjs';
+import { captureOneImage, waitForFrames, runScene, testCase, testClass, PlatformEnum } from 'db://automation-framework/runtime/test-framework.mjs';
 
 @runScene('label-outline')
 @testClass('LabelOutline', undefined, [PlatformEnum.WINDOWS])
 export class LabelOutline {
     @testCase
     async startPlay() {
-        await waitForNextFrame();
+        await waitForFrames();
         await captureOneImage();
     }
 }
