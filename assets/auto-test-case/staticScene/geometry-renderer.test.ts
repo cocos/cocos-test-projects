ca5f25e01c223b4c2ebef85289b89a9b60eb535f
练素琼 <suqiong.lian@cocos.com>
2023-03-02 14:52:31 +0800

    fomat code

--- assets/auto-test-case/staticScene/geometry-renderer.test.ts
+++ assets/auto-test-case/staticScene/geometry-renderer.test.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import {  find, Toggle } from "cc";
+import { find, Toggle } from "cc";
 import { runScene, testCase, testClass, beforeClass } from "db://automation-framework/runtime/test-framework.mjs";
 import { screenshot_custom } from "../dynamic/common/utils";
 
@@ -7,18 +7,18 @@ import { screenshot_custom } from "../dynamic/common/utils";
 @testClass('GeometryRenderer')
 export class GeometryRenderer {
     dt = 40;
-    toggle:  Toggle | undefined;
+    toggle: Toggle | undefined;
     geometryRenderer: Toggle | undefined;
     depthTest: Toggle | undefined;
     toggleUnlit: Toggle | undefined;
     toggleRotate: Toggle | undefined;
-    
+
     @beforeClass
-    async initData(){
-      this.geometryRenderer = this.getToggle("Canvas/ToggleWireframe", "cc.Toggle");
-      this.depthTest = this.getToggle("Canvas/ToggleDepthTest", "cc.Toggle");
-      this.toggleUnlit = this.getToggle("Canvas/ToggleUnlit", "cc.Toggle");
-      this.toggleRotate = this.getToggle("Canvas/ToggleRotate", "cc.Toggle");
+    async initData() {
+        this.geometryRenderer = this.getToggle("Canvas/ToggleWireframe", "cc.Toggle");
+        this.depthTest = this.getToggle("Canvas/ToggleDepthTest", "cc.Toggle");
+        this.toggleUnlit = this.getToggle("Canvas/ToggleUnlit", "cc.Toggle");
+        this.toggleRotate = this.getToggle("Canvas/ToggleRotate", "cc.Toggle");
     }
 
     @testCase
@@ -27,62 +27,62 @@ export class GeometryRenderer {
     }
 
     @testCase
-    async wireframe(){
+    async wireframe() {
         this.geometryRenderer!.isChecked = true;
         await screenshot_custom();
         this.geometryRenderer!.isChecked = false;
     }
 
     @testCase
-    async depthTests(){
-        this.depthTest!.isChecked=true;
+    async depthTests() {
+        this.depthTest!.isChecked = true;
         await screenshot_custom();
         this.depthTest!.isChecked = false;
     }
 
     @testCase
-    async toggleUnlits(){
-        this.toggleUnlit!.isChecked=true;
+    async toggleUnlits() {
+        this.toggleUnlit!.isChecked = true;
         await screenshot_custom();
-        this.toggleUnlit!.isChecked=false;
+        this.toggleUnlit!.isChecked = false;
     }
 
     @testCase
-    async toggleRotates(){
-        this.toggleRotate!.isChecked=true;
+    async toggleRotates() {
+        this.toggleRotate!.isChecked = true;
         await screenshot_custom();
-        for(let i=0; i <2; i++){
-            await screenshot_custom(this.dt-15);
+        for (let i = 0; i < 2; i++) {
+            await screenshot_custom(this.dt - 15);
         }
         await screenshot_custom(this.dt);
-        this.toggleRotate!.isChecked=false;
+        this.toggleRotate!.isChecked = false;
     }
 
     @testCase
-    async checkAll(){
+    async checkAll() {
         this.geometryRenderer!.isChecked = true;
-        this.depthTest!.isChecked=true;
-        this.toggleUnlit!.isChecked=true;
-        this.toggleRotate!.isChecked=true;
+        this.depthTest!.isChecked = true;
+        this.toggleUnlit!.isChecked = true;
+        this.toggleRotate!.isChecked = true;
         await screenshot_custom();
     }
 
-    getToggle(node: string, componentName: string){
+    getToggle(node: string, componentName: string) {
         //@ts-ignore
         this.toggle = find(node).getComponent(componentName);
-        if(!this.toggle){
+        if (!this.toggle) {
             this.retry(node, componentName, 3);
         }
         return this.toggle;
     }
 
-    retry(node: string, componentName: string, tryNum?: number){
-        for(let i=0; i < tryNum!; i++){
+    retry(node: string, componentName: string, tryNum?: number) {
+        for (let i = 0; i < tryNum!; i++) {
             //@ts-ignore
             this.toggle = find(node).getComponent(componentName);
-            if(this.toggle){
+            if (this.toggle) {
                 return;
-            }else{
+            } else {
                 continue;
             }
         }
--- assets/auto-test-case/staticScene/label-outline.test.ts
+++ assets/auto-test-case/staticScene/label-outline.test.ts
@@ -2,7 +2,7 @@
 import { captureOneImage, waitForNextFrame, runScene, testCase, testClass, PlatformEnum } from 'db://automation-framework/runtime/test-framework.mjs';
 
 @runScene('label-outline')
-@testClass('LabelOutline',undefined, [PlatformEnum.WINDOWS])
+@testClass('LabelOutline', undefined, [PlatformEnum.WINDOWS])
 export class LabelOutline {
     @testCase
     async startPlay() {
