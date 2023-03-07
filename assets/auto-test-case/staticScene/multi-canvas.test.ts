d56e6e39f5aba460444267d9ab4ea113172d93ae
lsq4590578 <lsq_4590578@126.com>
2023-03-07 16:27:34 +0800

    modify static-ui and multi-canvas

--- assets/auto-test-case/staticScene/multi-canvas.test.ts
+++ assets/auto-test-case/staticScene/multi-canvas.test.ts
@@ -1,31 +1,33 @@
 // @ts-ignore
-import { captureOneImage, waitForNextFrame, runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { screenshot_custom } from '../dynamic/common/utils';
+import { find } from 'cc';
 
 @runScene('multi-canvas')
 @testClass('MultiCanvas')
 export class MultiCanvas {
-    _dt = 5
+    _dt = 10
+
     @testCase
     async startPlay() {
-        await waitForNextFrame();
-        await captureOneImage();
+        await screenshot_custom();
     }
 
-    // @testCase
-    // async clickLeftButton() {
-    //     for (let i = 0; i < 5; i++) {
-    //         //@ts-ignore
-    //         find(`Canvas${i}/Button0-${i}`).getComponent('cc.Button').clickEvents[0].emit([]);
-    //         await screenshot_custom(this._dt)
-    //     }
-    // }
+    @testCase
+    async clickLeftButton() {
+        for (let i = 0; i < 5; i++) {
+            //@ts-ignore
+            find(`Canvas${i}/Button0-${i}`).getComponent('cc.Button').clickEvents[0].emit([]);
+            await screenshot_custom(this._dt);
+        }
+    }
 
-    // @testCase
-    // async clickRightButton() {
-    //     for (let i = 0; i < 5; i++) {
-    //         //@ts-ignore
-    //         find(`CanvasP${i}/Button1-${i}`).getComponent('ClickEvent').onButtonClick();
-    //         await screenshot_custom(this._dt)
-    //     }
-    // }
+    @testCase
+    async clickRightButton() {
+        for (let i = 0; i < 5; i++) {
+            //@ts-ignore
+            find(`CanvasP${i}/Button1-${i}`).getComponent('ClickEvent').onButtonClick();
+            await screenshot_custom(this._dt);
+        }
+    }
 }
