105e16df6790e38153222b9a45d6bf20ab31b971
linzihuang <zihuang.lin@chukong-inc.com>
2023-03-24 13:40:23 +0800

    修复case：SpriteAttributeSwitch

--- assets/auto-test-case/dynamic/ui/sprite-attribute-switch.test.ts
+++ assets/auto-test-case/dynamic/ui/sprite-attribute-switch.test.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { find, Button, Color } from 'cc';
+import { find, Button, Color, Size } from 'cc';
 import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 
@@ -17,13 +17,22 @@ export class SpriteAttributeSwitch {
       find('Canvas/buttons/enabledButton')!.getComponent(Button)?.clickEvents[0].emit([]);
       await screenshot_custom(this._dt);
       find('Canvas/buttons/resetButton')!.getComponent(Button)?.clickEvents[0].emit([]);
-      find('Canvas/buttons/contentSizeButton')!.getComponent(Button)?.clickEvents[0].emit([]);
+      find('Canvas')!.getComponent("SpriteAttributeSwitch")!.transform.contentSize = new Size(200 * 2 * 0.5, 200 * 2 * 0.4);
+      find('Canvas')!.getComponent("SpriteAttributeSwitch")!.updateState();
+      //find('Canvas/buttons/contentSizeButton')!.getComponent(Button)?.clickEvents[0].emit([]);
+
       await screenshot_custom(this._dt);
       find('Canvas/buttons/resetButton')!.getComponent(Button)?.clickEvents[0].emit([]);
-      find('Canvas/buttons/AnchorXButton')!.getComponent(Button)?.clickEvents[0].emit([]);
+      find('Canvas')!.getComponent("SpriteAttributeSwitch")!.transform.anchorX = 0.5;
+      find('Canvas')!.getComponent("SpriteAttributeSwitch")!.updateState();
+      //find('Canvas/buttons/AnchorXButton')!.getComponent(Button)?.clickEvents[0].emit([]);
+
+
       await screenshot_custom(this._dt);
       find('Canvas/buttons/resetButton')!.getComponent(Button)?.clickEvents[0].emit([]);
-      find('Canvas/buttons/AnchorYButton')!.getComponent(Button)?.clickEvents[0].emit([]);
+      find('Canvas')!.getComponent("SpriteAttributeSwitch")!.transform.anchorY = randY;
+      find('Canvas')!.getComponent("SpriteAttributeSwitch")!.updateState();
+      //find('Canvas/buttons/AnchorYButton')!.getComponent(Button)?.clickEvents[0].emit([]);
 
       // The color of the button changes randomly, and fixed parameters are provided here
       await screenshot_custom(this._dt);
