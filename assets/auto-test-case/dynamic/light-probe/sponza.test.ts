1d9db018fb97413ddd97e8d7854e234ebe43bdc5
lsq4590578 <lsq_4590578@126.com>
2023-03-30 15:08:05 +0800

    format code

--- assets/auto-test-case/dynamic/light-probe/sponza.test.ts
+++ assets/auto-test-case/dynamic/light-probe/sponza.test.ts
@@ -6,7 +6,7 @@ import { screenshot_custom } from '../common/utils';
 
 @runScene("sponza")
 @testClass("Sponza", undefined, PlatformEnum.WEB_DESKTOP, PlatformEnum.WEB_MOBILE, PlatformEnum.WECHATGAME, PlatformEnum.BYTEDANCE_MINI_GAME,
-PlatformEnum.OPPO_MINI_GAME,PlatformEnum.HUAWEI_QUICK_GAME,PlatformEnum.VIVO_MINI_GAME)
+    PlatformEnum.OPPO_MINI_GAME, PlatformEnum.HUAWEI_QUICK_GAME, PlatformEnum.VIVO_MINI_GAME)
 export class Sponza {
     tickTime: number = 120;
 
