c60268dc9c41f3feff3dc35f1c7c94606f43b1c4
胡志锋 <zhifeng.hu@cocos.com>
2023-05-29 17:29:45 +0800

    fix: terrain.test.ts 1、目标图需Triangle数据 2、需添加一张勾选按钮后的 3、部分平台需判断，如web-mobile勾选后Triangle数值要比未勾选的小才算测试通过(但部分平台不支持：如ios，勾不勾选数值不变化)

--- assets/auto-test-case/staticScene/terrain.test.ts
+++ assets/auto-test-case/staticScene/terrain.test.ts
@@ -1,12 +1,38 @@
 // @ts-ignore
-import { captureOneImage, waitForNextFrame, runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { Label, Toggle, find, sys } from 'cc';
+import { captureOneImage, waitForNextFrame, runScene, testCase, testClass, expect } from 'db://automation-framework/runtime/test-framework.mjs';
 
-@runScene('terrain')
-@testClass('Terrain')
+@testClass('Terrain', 'terrain')
 export class Terrain {
+
     @testCase
     async startPlay() {
         await waitForNextFrame();
         await captureOneImage();
+    }
+
+    @testCase
+    async lod() {
+        const toggle = find('Canvas/Toggle1')?.getComponent(Toggle) as Toggle;
+        expect(toggle).to.not.be.a('null');
+
+        const label = find('Canvas/TerrainTris')?.getComponent(Label) as Label;
+        expect(label).to.not.be.a('null');
+
+        const value1 = parseInt(label.string);
+
+        toggle.isChecked = true;
+        await waitForNextFrame(30);
+        await captureOneImage();
+
+        const value2 = parseInt(label.string);
+
+        toggle.isChecked = false;
+        await waitForNextFrame(30);
+        await captureOneImage();
+
+        if (sys.platform === sys.Platform.DESKTOP_BROWSER || sys.platform === sys.Platform.MOBILE_BROWSER) {
+            expect(value2, `勾选 Lod 后 Triangle（数值=${value2}）应比未勾选的（数值=${value1}）小`).to.be.below(value1);
+        }
     }
 }
