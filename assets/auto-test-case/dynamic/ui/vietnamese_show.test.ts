523ddb48dbfee4706b6aaee918d2f39547c9b442
胡志锋 <zhifeng.hu@cocos.com>
2023-05-30 11:00:46 +0800

    fix: vietnamese-show 截图优化：缺少滑动下拉图片

--- assets/auto-test-case/dynamic/ui/vietnamese_show.test.ts
+++ assets/auto-test-case/dynamic/ui/vietnamese_show.test.ts
@@ -1,41 +1,48 @@
-import { find, Button, Component } from 'cc';
+import { find, Button, Component, ScrollView } from 'cc';
 // @ts-ignore
-import { runScene, testCase, testClass, beforeClass, waitForFrames} from 'db://automation-framework/runtime/test-framework.mjs';
+import { runScene, testCase, testClass, beforeClass, waitForFrames, expect } from 'db://automation-framework/runtime/test-framework.mjs';
 import { VietnameseText } from '../../../cases/ui/02.label/vietnamese/VietnameseText';
 import { Test } from '../../../cases/ui/17.sprite-atlas/TS/Test';
 import { screenshot_custom } from '../common/utils';
 
-@runScene('vietnamese-show')
-@testClass('VietnameseShow')
+@testClass('VietnameseShow', 'vietnamese-show')
 export class VietnameseShow {
     _dt = 30;
 
-    testScript!: VietnameseText | Component | null;
-    isLoadButton = false;
+    testScript!: VietnameseText | null;
 
     @beforeClass
     async initData() {
-      this.testScript = find('Canvas')!.getComponent("VietnameseText");
+        this.testScript = find('Canvas')?.getComponent(VietnameseText) as VietnameseText;
     }
 
     @testCase
     async start() {
-      let num = 1000; //Used for counting frames, up to 1000 frames
-      let isLoadButton = this.testScript!.isLoadButton;
-      while(!isLoadButton && num>0){
-          num -= 1;
-          isLoadButton = this.testScript!.isLoadButton;
-          await waitForFrames(1); 
-      }
-      
-      await screenshot_custom(this._dt);
-      let btns = this.testScript.btnContainer.children;
-      if(btns && btns.length>1){
-        for(let i=1; i<btns.length; i++){
-          let ele = btns[i];
-          ele.emit(Button.EventType.CLICK);
-          await screenshot_custom(this._dt);
+        expect(this.testScript).to.not.be.a('null');
+
+        let num = 10;
+        while (!this.testScript!.isLoadButton && num > 0) {
+            num -= 1;
+            await waitForFrames(60);
         }
-      }
+
+        expect(this.testScript!.isLoadButton).to.equal(true);
+
+        await screenshot_custom(this._dt);
+
+        const btns = this.testScript!.btnContainer.children;
+        const scrollView = find('Canvas/ScrollView')!.getComponent(ScrollView)!;
+        for (let i = 1; i < btns.length; i++) {
+            let ele = btns[i];
+            ele.emit(Button.EventType.CLICK);
+            await waitForFrames(1);
+            await screenshot_custom(this._dt);
+
+            if (i === 1 || i === 8) {
+                scrollView.scrollToBottom(0);
+                await waitForFrames(1);
+                await screenshot_custom(this._dt);
+            }
+        }
     }
 }
