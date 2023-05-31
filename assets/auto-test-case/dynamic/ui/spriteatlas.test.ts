4c70dcdf6d228f1f75ed5e9c512310a5a9d03c77
胡志锋 <zhifeng.hu@cocos.com>
2023-05-31 11:19:13 +0800

    fix: spriteatlas.test.ts 目标图需优化，优化点如下a、b、c

--- assets/auto-test-case/dynamic/ui/spriteatlas.test.ts
+++ assets/auto-test-case/dynamic/ui/spriteatlas.test.ts
@@ -4,31 +4,66 @@ import { runScene, testCase, testClass, beforeClass } from 'db://automation-fram
 import { Test } from '../../../cases/ui/17.sprite-atlas/TS/Test';
 import { screenshot_custom } from '../common/utils';
 
-@runScene('spriteatlas')
-@testClass('Spriteatlas')
+@testClass('Spriteatlas', 'spriteatlas')
 export class Spriteatlas {
-    _dt = 30;
+    _dt = 10;
 
-    testScript!: Test | Component | null;
+    testScript!: Test | null;
 
     @beforeClass
     async initData() {
-      this.testScript = find('Canvas/Sprite')!.getComponent("Test")
+      this.testScript = find('Canvas/Sprite')!.getComponent("Test") as Test;
     }
 
     @testCase
     async start() {
+      const buttonChange = find('Canvas/Button-1')!.getComponent(Button)!;
+      const buttonClean = find('Canvas/Button-2')!.getComponent(Button)!;
+      const buttonReplace = find('Canvas/Button-3')!.getComponent(Button)!;
+      const buttonTips = find('Canvas/Button-4')!.getComponent(Button)!;
+
       await screenshot_custom(this._dt);
+
       this.testScript!.editbox.string = "lake";
-      find('Canvas/Button-1')!.getComponent(Button)?.clickEvents[0].emit([]);
+      buttonChange.clickEvents[0].emit([]);
       await screenshot_custom(this._dt);
-      find('Canvas/Button-3')!.getComponent(Button)?.clickEvents[0].emit([]);
+
+      this.testScript!.editbox.string = "mountain";
+      buttonChange.clickEvents[0].emit([]);
       await screenshot_custom(this._dt);
-      find('Canvas/Button-2')!.getComponent(Button)?.clickEvents[0].emit([]);
+
+      this.testScript!.editbox.string = "wrong";
+      buttonChange.clickEvents[0].emit([]);
       await screenshot_custom(this._dt);
-      find('Canvas/Button-1')!.getComponent(Button)?.clickEvents[0].emit([]);
+
+
+      this.testScript!.editbox.string = "sea";
+      buttonReplace.clickEvents[0].emit([]);
       await screenshot_custom(this._dt);
-      find('Canvas/Button-4')!.getComponent(Button)?.clickEvents[0].emit([]);
+
+      this.testScript!.editbox.string = "tree";
+      buttonReplace.clickEvents[0].emit([]);
+      await screenshot_custom(this._dt);
+
+      this.testScript!.editbox.string = "wrong";
+      buttonReplace.clickEvents[0].emit([]);
+      await screenshot_custom(this._dt);
+
+
+      buttonClean.clickEvents[0].emit([]);
+      await screenshot_custom(this._dt);
+
+      this.testScript!.editbox.string = "lake";
+      buttonChange.clickEvents[0].emit([]);
+      await screenshot_custom(this._dt);
+      buttonReplace.clickEvents[0].emit([]);
+      await screenshot_custom(this._dt);
+
+
+      buttonTips.clickEvents[0].emit([]);
+      await screenshot_custom(this._dt);
+
+      buttonTips.clickEvents[0].emit([]);
       await screenshot_custom(this._dt);
     }
 }
