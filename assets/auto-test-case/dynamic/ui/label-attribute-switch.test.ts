4da629bfdc16fcbb5830f46169c42befe7e628dd
胡志锋 <zhifeng.hu@cocos.com>
2023-06-01 14:54:03 +0800

    fix: label-attribute-switch 目标图优化

--- assets/auto-test-case/dynamic/ui/label-attribute-switch.test.ts
+++ assets/auto-test-case/dynamic/ui/label-attribute-switch.test.ts
@@ -1,26 +1,28 @@
 // @ts-ignore
-import { Color, Component, find, Label, LabelOutline, LabelShadow, Size, UITransform, Vec2 } from 'cc';
+import { Button, Color, Component, find, Label, LabelOutline, LabelShadow, Size, UITransform, Vec2 } from 'cc';
 // @ts-ignore
 import { runScene, testCase, testClass, beforeClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 
-@runScene('label-attribute-switch')
-@testClass('LabelAttributeSwitch')
+@testClass('LabelAttributeSwitch', 'label-attribute-switch')
 export class LabelAttributeSwitch {
     tickTime: number = 30;
-    transform!: UITransform | Component | null;
     oriSize: Size = new Size(200, 200);
-    labelAttribute!: Component;
-    label!: Label | Component;
-    outline!: LabelOutline | Component;
-    shadow!: LabelShadow | Component;
+    caseScript!: any;
+    resetBtn!: Button | null;
+    label!: Label;
+    transform!: UITransform | null;
+    outline!: LabelOutline;
+    shadow!: LabelShadow;
 
     @beforeClass
     initData() {
-        this.transform = find("Canvas/Label")!.getComponent("cc.UITransform")!;
-        this.label = find("Canvas/Label")!.getComponent("cc.Label")!;
-        this.outline = find("Canvas/Label")!.getComponent("cc.LabelOutline")!;
-        this.shadow = find("Canvas/Label")!.getComponent("cc.LabelShadow")!;
+        this.caseScript = find("Canvas")!.getComponent("LabelAttributeSwitch")!;
+        this.resetBtn = find("Canvas/buttons/resetButton")!.getComponent("cc.Button") as Button;
+        this.label = find("Canvas/Label")!.getComponent("cc.Label") as Label;
+        this.transform = find("Canvas/Label")!.getComponent("cc.UITransform") as UITransform;
+        this.outline = find("Canvas/Label")!.getComponent("cc.LabelOutline") as LabelOutline;
+        this.shadow = find("Canvas/Label")!.getComponent("cc.LabelShadow") as LabelShadow;
     }
 
     @testCase
@@ -30,18 +32,18 @@ export class LabelAttributeSwitch {
 
     @testCase
     async activeButton() {
-        const activeBtn = find("Canvas/buttons/activeButton")!.getComponent("cc.Button");
+        const btn = find("Canvas/buttons/activeButton")!.getComponent("cc.Button") as Button;
         for (let i = 0; i < 2; i++) {
-            activeBtn!.clickEvents[0].emit([]);
+            btn!.clickEvents[0].emit([]);
             await screenshot_custom(this.tickTime);
         }
     }
 
     @testCase
     async enabledButton() {
-        const activeBtn = find("Canvas/buttons/enabledButton")!.getComponent("cc.Button");
+        const btn = find("Canvas/buttons/enabledButton")!.getComponent("cc.Button") as Button;
         for (let i = 0; i < 2; i++) {
-            activeBtn!.clickEvents[0].emit([]);
+            btn!.clickEvents[0].emit([]);
             await screenshot_custom(this.tickTime);
         }
     }
@@ -52,188 +54,202 @@ export class LabelAttributeSwitch {
         const randY = 0.19348385088486952;
         //contentSizeButton!.clickEvents[0].emit([]);
         this.transform!.contentSize = new Size(this.oriSize.x * 2 * randX, this.oriSize.y * 2 * randY);
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
 
         const x = 0.2945661979873351;
         const y = 0.7921869604254592;
         this.transform!.contentSize = new Size(this.oriSize.x * 2 * x, this.oriSize.y * 2 * y);
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async switchAnchorX() {
+        this.reset();
         const randX = 0.6521365915694648;
-        const randY = 0.5;
         this.transform!.anchorX = randX;
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async switchAnchorY() {
+        this.reset();
         const randY = 0.8641434767227314;
-        this.transform!.randY = randY;
-        this.updateState();
+        this.transform!.anchorY = randY;
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async colorButton() {
+        this.reset();
         const color = new Color(255 * 0.3, 255 * 0.5, 255 * 0.4, 255 * 0.8);
         this.label!.color = color;
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async stringButton() {
-        this.label!.string = "testing string";
-        this.updateState();
+        this.reset();
+        this.label!.string = "testing string 1";
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
 
-        this.label!.string = "testing string wrjwqsjdlsakjdsaldsdjsldsaldsjdksajdsakjdsadsadsadsad";
-        this.updateState();
+        this.label!.string = "testing string 2 hello";
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
 
     }
 
     @testCase
     async horizontalAlignButton() {
-        const activeBtn = find("Canvas/buttons/horizontalAlignButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
+        this.reset();
+        const btn = find("Canvas/buttons/horizontalAlignButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
     @testCase
     async verticalAlignButton() {
-        const activeBtn = find("Canvas/buttons/verticalAlignButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
+        this.reset();
+        const btn = find("Canvas/buttons/verticalAlignButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
     @testCase
     async fontButton() {
-        const activeBtn = find("Canvas/buttons/fontButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
-        await screenshot_custom(this.tickTime);
-
-        activeBtn!.clickEvents[0].emit([]);
+        this.reset();
+        const btn = find("Canvas/buttons/fontButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async useSystemFontButton() {
-        const activeBtn = find("Canvas/buttons/useSystemFontButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
-        await screenshot_custom(this.tickTime);
-        //use system font button
-        activeBtn!.clickEvents[0].emit([]);
+        this.reset();
+        const btn = find("Canvas/buttons/useSystemFontButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
     @testCase
     async fontSizeButton() {
+        this.reset();
         this.label!.fontSize = 20;
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
-
 
     @testCase
     async overflowButton() {
-        const activeBtn = find("Canvas/buttons/overflowButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
+        this.reset();
+        const btn = find("Canvas/buttons/overflowButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
+        await screenshot_custom(this.tickTime);
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
-
-
 
     @testCase
     async cacheModeButton() {
-        const activeBtn: Component | null = find("Canvas/buttons/cacheModeButton")!.getComponent("cc.Button");
+        this.reset();
+        const btn = find("Canvas/buttons/cacheModeButton")!.getComponent("cc.Button") as Button;
         //mode:0
-        activeBtn!.clickEvents[0].emit([]);
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
         //mode:1
-        activeBtn!.clickEvents[0].emit([]);
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
         //mode:2
-        activeBtn!.clickEvents[0].emit([]);
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
-
     @testCase
     async outlineButton() {
-        const activeBtn = find("Canvas/buttons/outlineButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
+        this.reset();
+        const btn = find("Canvas/buttons/outlineButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
     @testCase
     async outlineColorButton() {
+        this.reset();
         this.outline.color = new Color(255 * 0.3722029865090255, 255 * 0.5710897253179028, 255 * 0.8497690059890528, 255 * 0.3513002481020939);
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async switchOutlineWidth() {
+        this.reset();
         this.outline.width = 4.898001506301493;
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
     @testCase
     async shadowButton() {
-        this.shadow.enabled = !this.shadow.enabled;
-        this.updateState();
+        this.reset();
+        const btn = find("Canvas/buttons/shadowButton")!.getComponent("cc.Button") as Button;
+        btn!.clickEvents[0].emit([]);
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async shadowColorButton() {
+        this.reset();
         this.shadow.color = new Color(255 * 0.8662123185692148, 255 * 0.15273869974057686, 255 * 0.7999636186648476, 255 * 0.9266380103869918);
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async shadowOffsetButton() {
+        this.reset();
         this.shadow.offset = new Vec2(3 * 2 * 0.8943199701432856, 3 * 2 * 0.3134031421313581);
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
     @testCase
     async shadowBlurButton() {
+        this.reset();
         this.shadow.blur = 2.382617145815883;
-        this.updateState();
+        this.caseScript.updateState();
         await screenshot_custom(this.tickTime);
     }
 
 
-    @testCase
-    async reset() {
-        const activeBtn = find("Canvas/buttons/resetButton")!.getComponent("cc.Button");
-        activeBtn!.clickEvents[0].emit([]);
-        await screenshot_custom(this.tickTime);
+    reset() {
+        this.resetBtn!.clickEvents[0].emit([]);
+        // await screenshot_custom(this.tickTime);
     }
 
-    updateState() {
-        //@ts-ignore
-        this.labelAttribute = find("Canvas")!.getComponent("LabelAttributeSwitch")!;
-        this.labelAttribute.updateState();
-    }
 }
