8f7861a0ac68a365dfe71aa79e0fcec19bf90ad6
胡志锋 <zhifeng.hu@cocos.com>
2023-06-15 12:36:52 +0800

    fix PlaybackRange.test.ts  自动化跑的截图跟预览的效果不一致

--- assets/auto-test-case/dynamic/animation/PlaybackRange.test.ts
+++ assets/auto-test-case/dynamic/animation/PlaybackRange.test.ts
@@ -1,95 +1,76 @@
-import { find } from 'cc';
+import { EventHandler, Label, find } from 'cc';
 // @ts-ignore
-import { runScene, testCase, testClass, sleep } from 'db://automation-framework/runtime/test-framework.mjs';
-import { screenshot_custom,screenshot_custom_by_wait} from '../common/utils';
+import { testClass, testCase, beforeClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { screenshot_custom, screenshot_custom_by_wait} from '../common/utils';
+import { PlaybackRange } from '../../../cases/animation/scripts/PlaybackRange';
 
-@runScene('PlaybackRange')
-@testClass('PlaybackRange')
-export class PlaybackRange {
-    _dt = 92
-    _delay = 2;
+@testClass('PlaybackRange', 'PlaybackRange')
+export class PlaybackRange2 {
+    private caseScript!: PlaybackRange;
+    private max!: number;
+
+    @beforeClass
+    async initData() {
+        this.caseScript = find('scene')!.getComponent('PlaybackRange') as PlaybackRange;
+    }
 
     @testCase
     async startPlay() {
         await screenshot_custom();
     }
 
+    // 0~210
     @testCase
     async headLeftRightWalk(){
-        // @ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.sliderMin.progress = 0;
-        this.setMinLabelString();
-        // @ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.sliderMax.progress = 0.3;
-        this.setMaxLabelString();
-        //head left
-        await screenshot_custom(this._dt);
-        //head right
-        await screenshot_custom_by_wait(82);
-        //walk one picture
-        await screenshot_custom_by_wait(258);
-        //walk two picture
-        await screenshot_custom_by_wait(18);
+        this.max = this.caseScript.labeledSliderMax.max;
 
+        this.setMinLabelString(0);
+        this.setMaxLabelString(210/this.max);
+
+        await screenshot_custom_by_wait(92);
+        await screenshot_custom_by_wait(82);
     }
 
-
+    // 210-322
     @testCase
     async walk(){
-        // @ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.sliderMin.progress = 0.3;
-        this.setMinLabelString();
-        // @ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.sliderMax.progress = 0.7;
-        this.setMaxLabelString();
-        //walk three picture
-        await screenshot_custom(this._dt+4);
+        this.setMinLabelString(210/this.max);
+        this.setMaxLabelString(322/this.max);
+
+        await screenshot_custom_by_wait(10);
+        await screenshot_custom_by_wait(40);
+        await screenshot_custom_by_wait(55);
     }
 
+    // 322-this.max
     @testCase
     async run() {
-        // @ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.sliderMin.progress = 0.7;
-        this.setMinLabelString();
-        // @ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.sliderMax.progress = 1;
-        this.setMaxLabelString();
-        await screenshot_custom_by_wait(130);
+        this.setMinLabelString(322/this.max);
+        this.setMaxLabelString(1);
+
+        await screenshot_custom_by_wait(10);
         await screenshot_custom_by_wait(30);
         await screenshot_custom_by_wait(20);
         await screenshot_custom_by_wait(20);
         await screenshot_custom_by_wait(20);
-        await screenshot_custom_by_wait(this._dt*3-12);
+        await screenshot_custom_by_wait(20);
     }
 
-    //init data
     getInitData() {
-        // @ts-ignore
-        let min = find('scene')!.getComponent('PlaybackRange')!.labeledSliderMin.min;
-        // @ts-ignore
-        let max = find('scene')!.getComponent('PlaybackRange')!.labeledSliderMax.max;
+        let min = this.caseScript.labeledSliderMin.min;
+        let max = this.caseScript.labeledSliderMax.max;
         return [min, max]
     }
 
-    setMaxLabelString() {
-        //@ts-ignore
-        let integral = find('scene')!.getComponent('PlaybackRange')!.labeledSliderMax.integral;
-        // @ts-ignore
-        let progress = find('scene')!.getComponent('PlaybackRange')!.sliderMax.progress;
-        let [min, max] = this.getInitData();
-        const val = min + (max - min) * progress;
-        //@ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.labeledSliderMax._valueLabel.string = `${integral ? Math.floor(val) : val}`;
+    setMaxLabelString(value: number) {
+        this.caseScript.sliderMax.progress = value;
+        EventHandler.emitEvents(this.caseScript.sliderMax.slideEvents, this.caseScript.sliderMax);
+        this.caseScript.sliderMax.node.emit('slide', this.caseScript.sliderMax);
     }
 
-    setMinLabelString() {
-        //@ts-ignore
-        let integral = find('scene')!.getComponent('PlaybackRange')!.labeledSliderMin.integral;
-        // @ts-ignore
-        let progress = find('scene')!.getComponent('PlaybackRange')!.sliderMin.progress;
-        let [min, max] = this.getInitData();
-        const val = min + (max - min) * progress;
-        //@ts-ignore
-        find('scene')!.getComponent('PlaybackRange')!.labeledSliderMin._valueLabel.string = `${integral ? Math.floor(val) : val}`;
+    setMinLabelString(value: number) {
+        this.caseScript.sliderMin.progress = value;
+        EventHandler.emitEvents(this.caseScript.sliderMin.slideEvents, this.caseScript.sliderMin);
+        this.caseScript.sliderMin.node.emit('slide', this.caseScript.sliderMin);
     }
 }
