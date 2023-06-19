fcacd2bec711ae22a71fff8a8924cdc02320d056
胡志锋 <zhifeng.hu@cocos.com>
2023-06-19 18:33:49 +0800

    fix widget-performance.test.ts

--- assets/auto-test-case/dynamic/ui/widget-performance.test.ts
+++ assets/auto-test-case/dynamic/ui/widget-performance.test.ts
@@ -1,17 +1,46 @@
 // @ts-ignore
-import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { beforeClass, afterClass, testCase, testClass, waitForFrames, expect } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
+import { Label, find, profiler } from 'cc';
 
-@runScene('widget-performance')
-// @testClass('WidgetPerformance')
+@testClass('WidgetPerformance', 'widget-performance')
 export class WidgetPerformance {
-    _dt = 50;
+    private tip!: Label | null;
+    private now!: number;
+    private fps: number | undefined = undefined;
+
+    @beforeClass
+    async initData() {
+        profiler.showStats();
+        this.tip = find('canvas/tip')?.getComponent(Label) as Label;
+    }
+
+    @afterClass
+    async destoryData() {
+        profiler.hideStats();
+    }
 
     @testCase
     async startPlay() {
-        for (let i = 0; i < 5; i++) {
-            await screenshot_custom(this._dt);
-        };
+        this.now = performance.now();
+        await screenshot_custom();
     }
 
+    @testCase
+    async adjustWidget() {
+        for (let now2, fps2, i = 0; i < 9; i++) {
+            await waitForFrames(29);
+            now2 = performance.now();
+            fps2 = Math.round(30 / (now2 - this.now) * 1000);
+            this.tip!.string = `FPS: ${fps2}`;
+            await screenshot_custom();
+
+            if (i > 1 && this.fps !== undefined) {
+                expect(Math.abs((fps2 - this.fps) / this.fps)).to.lessThan(0.2, `FPS前后波动过大！前：${this.fps} 后：${fps2}`);
+            }
+
+            this.now = now2;
+            this.fps = fps2;
+        }
+    }
 }
