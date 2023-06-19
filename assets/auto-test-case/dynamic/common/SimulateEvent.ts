f78113352512831c2f1fd7d9df5403bbe84bee14
胡志锋 <zhifeng.hu@cocos.com>
2023-06-19 16:20:23 +0800

    add click-when-scroll.test.ts

--- assets/auto-test-case/dynamic/common/SimulateEvent.ts
+++ assets/auto-test-case/dynamic/common/SimulateEvent.ts
@@ -29,13 +29,16 @@ export function simulateMultiTouch(startX: number, startY: number, moveX: number
 
 
 //模拟结束
-export function simulateTouchEnd(node?: Node) {
-    let event = new EventTouch([], true, Input.EventType.TOUCH_END, []);
+export function simulateTouchEnd(node?: Node, x: number = 0, y: number = 0) {
+    const changedTouches: Touch[] = [];
+    let event = new EventTouch(changedTouches, true, Input.EventType.TOUCH_END, []);
     if (node) {
+        event.target = event.currentTarget = node;
+        event.touch = new Touch(x, y, 0);
+        changedTouches.push(event.touch);
         node!.dispatchEvent(event);
     }
     return event;
-
 }
 
 //鼠标事件
---
+++ assets/auto-test-case/dynamic/ui/click-when-scroll.test.ts
@@ -0,0 +1,29 @@
+// @ts-ignore
+import { testCase, testClass, waitForFrames } from 'db://automation-framework/runtime/test-framework.mjs';
+import { screenshot_custom_by_wait } from '../common/utils';
+import { input, Input, find, Touch, EventTouch, Event, EventMouse } from 'cc';
+import { simulateTouchStart, simulateTouchEnd, simulateMultiTouch } from '../common/SimulateEvent';
+
+@testClass('ClickWhenScroll', 'click-when-scroll')
+export class ClickWhenScroll {
+    @testCase
+    async startPlay() {
+        await screenshot_custom_by_wait(5);
+    }
+
+    @testCase
+    async clickItem() {
+        const contentNode = find('Canvas/New ScrollView/view/content');
+
+        for (let item, i=0; i<7; i++) {
+            if (i === 5) {
+                simulateTouchEnd(item, 0, 200);
+                await waitForFrames(60);
+            }
+            item = contentNode?.children[i];
+            simulateTouchStart(0, 0, item);
+            simulateTouchEnd(item, 0, 0);
+            await screenshot_custom_by_wait(5);
+        }
+    }
+}
