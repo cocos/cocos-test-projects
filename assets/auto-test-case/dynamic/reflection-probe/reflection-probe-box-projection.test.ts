9e15350a22c702b3f8d721c8ddcde32185c33a55
胡志锋 <zhifeng.hu@cocos.com>
2023-06-25 17:31:44 +0800

    update reflection-probe-box-projection.test.ts

--- assets/auto-test-case/dynamic/reflection-probe/reflection-probe-box-projection.test.ts
+++ assets/auto-test-case/dynamic/reflection-probe/reflection-probe-box-projection.test.ts
@@ -1,17 +1,16 @@
 // @ts-ignore
-import { runScene, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 
-@runScene('reflection-probe-box-projection')
-@testClass('ReflectionProbeBoxProjection')
+@testClass('ReflectionProbeBoxProjection', 'reflection-probe-box-projection')
 export class ReflectionProbeBoxProjection {
-    _dt = 60;
+    _dt = 120;
 
     @testCase
     async start() {
+      await screenshot_custom(2);
       await screenshot_custom(this._dt);
-      await screenshot_custom(this._dt*4);
-      await screenshot_custom(this._dt*6);
-      await screenshot_custom(this._dt*10);
+      await screenshot_custom(this._dt);
+      await screenshot_custom(this._dt);
     }
 }
