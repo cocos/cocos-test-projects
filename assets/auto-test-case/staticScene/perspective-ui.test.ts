88fa050e2f757deb4bc283e02596ee7b063a3a42
胡志锋 <zhifeng.hu@cocos.com>
2023-06-25 17:16:35 +0800

    update perspective-ui.test.ts

--- assets/auto-test-case/staticScene/perspective-ui.test.ts
+++ assets/auto-test-case/staticScene/perspective-ui.test.ts
@@ -1,16 +1,11 @@
-import { find } from 'cc';
 // @ts-ignore
-import { runScene, testCase, testClass, sleep } from 'db://automation-framework/runtime/test-framework.mjs';
+import { testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../dynamic/common/utils';
 
-@runScene('perspective-ui')
-//@testClass('Perspectiveui')
+@testClass('Perspectiveui', 'perspective-ui')
 export class Perspectiveui {
-    _dt = 10;
-
     @testCase
-    async start(){
-        await sleep(0.5)
-        await screenshot_custom(this._dt);
+    async start() {
+        await screenshot_custom(2);
     }
 }
