448c43a27107f1b1e5458a7265e01672a3d5988a
胡志锋 <zhifeng.hu@cocos.com>
2023-06-26 09:42:41 +0800

    add lod.test.ts

--- assets/auto-test-case/dynamic/reflection-probe/reflection-probe-box-projection.test.ts
+++ assets/auto-test-case/dynamic/reflection-probe/reflection-probe-box-projection.test.ts
@@ -4,13 +4,13 @@ import { screenshot_custom } from '../common/utils';
 
 @testClass('ReflectionProbeBoxProjection', 'reflection-probe-box-projection')
 export class ReflectionProbeBoxProjection {
-    _dt = 120;
+    private df = 120;
 
     @testCase
     async start() {
       await screenshot_custom(2);
-      await screenshot_custom(this._dt);
-      await screenshot_custom(this._dt);
-      await screenshot_custom(this._dt);
+      await screenshot_custom(this.df);
+      await screenshot_custom(this.df);
+      await screenshot_custom(this.df);
     }
 }
