3334128db394c828921e056d55cc2f2ad147b046
胡志锋 <zhifeng.hu@cocos.com>
2023-07-14 11:19:37 +0800

    add particle-culling.test.ts

--- assets/auto-test-case/dynamic/particle/particle-culling.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-culling.test.ts
@@ -4,21 +4,28 @@ import { testCase, testClass, beforeClass, srandom } from 'db://automation-frame
 import { screenshot_custom_by_wait } from '../common/utils';
 import { FirstPersonCamera } from '../../../shared-res/first-person-camera';
 
-@testClass('ParticleColor', 'particle-color')
-export class ParticleColor {
+@testClass('ParticleCulling', 'particle-culling')
+export class ParticleCulling {
     private camera!: Node;
-    private df = 30;
+    private df = 60;
 
     @beforeClass
     async initData() {
-        this.camera = find('Camera')!;
+        this.camera = find('Main Camera')!;
         this.camera.getComponent(FirstPersonCamera)!.enabled = false;
         srandom('particle-color');
     }
 
     @testCase
     async startPlay() {
-        for (let i=0; i<6; i++) {
+        for (let i=0; i<3; i++) {
+            await screenshot_custom_by_wait(this.df);
+        }
+
+        this.camera.setPosition(4.127582478513955, -6.4638891333721356, 60.45422611717844);
+        this.camera.setRotation(0.1042203919870644, 0.07629875326349554, -0.008019322113484231, 0.9915907929268704);
+
+        for (let i=0; i<3; i++) {
             await screenshot_custom_by_wait(this.df);
         }
     }
