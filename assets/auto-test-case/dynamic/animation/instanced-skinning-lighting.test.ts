be0809a49b5923d3408b9f499f0b6a34a0959bc6
胡志锋 <zhifeng.hu@cocos.com>
2023-08-11 10:33:51 +0800

    instanced-skinning-lighting:缺少回到原点图片

--- assets/auto-test-case/dynamic/animation/instanced-skinning-lighting.test.ts
+++ assets/auto-test-case/dynamic/animation/instanced-skinning-lighting.test.ts
@@ -3,6 +3,7 @@ import { EventHandler, Slider, Toggle, find, Node } from 'cc';
 import { testCase, testClass, beforeClass } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 import { FirstPersonCamera } from '../../../shared-res/first-person-camera';
+import { UISimulate } from '../common/SimulateEvent';
 
 @testClass('InstancedSkinningLighting', 'instanced-skinning-lighting')
 export class InstancedSkinningLighting {
@@ -20,47 +21,42 @@ export class InstancedSkinningLighting {
 
     @testCase
     async start() {
-        await screenshot_custom(2);
+        UISimulate.changeSlider(this.slider, 0.5);
+        await screenshot_custom(30);
     }
 
     @testCase
     async setGroups1() {
-        this.slider.progress = 0.5;
-        EventHandler.emitEvents(this.slider.slideEvents, this.slider);
-        await screenshot_custom(30);
-
         this.camera.setPosition(72.89037919539744, 69.1681749364538, 122.07584548695787);
         this.camera.setRotation(-0.27354449145630166, 0.014855429665569025, 0.004225300622927272, 0.9617353452161412);
         await screenshot_custom(30);
 
-        this.slider.progress = 1;
-        EventHandler.emitEvents(this.slider.slideEvents, this.slider);
+        UISimulate.changeSlider(this.slider, 1);
         this.camera.setPosition(146.9864671233138, 122.52474055436167, 211.78317084154207);
         this.camera.setRotation(-0.26517307761640896, -0.0002533028105553636, -0.00006966296471365275, 0.9642007933476302);
         await screenshot_custom(120);
+
+        UISimulate.changeSlider(this.slider, 0);
+        await screenshot_custom(30);
     }
 
     @testCase
     async setGroups2() {
-        this.toggle.isChecked = true;
-        EventHandler.emitEvents(this.toggle.checkEvents, this.toggle);
+        UISimulate.changeToggle(this.toggle, true);
         
-        this.slider.progress = 0.02;
-        EventHandler.emitEvents(this.slider.slideEvents, this.slider);
+        UISimulate.changeSlider(this.slider, 0);
         this.camera.setPosition(19, 14, 24);
         this.camera.setRotation(-0.2140622116447128, 0.14430573790058746, 0.03199181188131125, 0.965572238378513);
         await screenshot_custom(30);
 
-        this.slider.progress = 0.5;
-        EventHandler.emitEvents(this.slider.slideEvents, this.slider);
+        UISimulate.changeSlider(this.slider, 0.5);
         await screenshot_custom(30);
 
         this.camera.setPosition(72.89037919539744, 69.1681749364538, 122.07584548695787);
         this.camera.setRotation(-0.27354449145630166, 0.014855429665569025, 0.004225300622927272, 0.9617353452161412);
         await screenshot_custom(30);
 
-        this.slider.progress = 1;
-        EventHandler.emitEvents(this.slider.slideEvents, this.slider);
+        UISimulate.changeSlider(this.slider, 1);
         this.camera.setPosition(146.9864671233138, 122.52474055436167, 211.78317084154207);
         this.camera.setRotation(-0.26517307761640896, -0.0002533028105553636, -0.00006966296471365275, 0.9642007933476302);
         await screenshot_custom(120);
