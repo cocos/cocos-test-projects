53ba3fad0ff4a109e6f2ad3c9e6e6f7223ca353a
胡志锋 <zhifeng.hu@cocos.com>
2023-08-25 14:50:05 +0800

    fix: particle-size ：截图时机有时候会改变

--- assets/auto-test-case/dynamic/particle/particle-size.test.ts
+++ assets/auto-test-case/dynamic/particle/particle-size.test.ts
@@ -1,12 +1,17 @@
-import { find } from 'cc';
+import { find, director } from 'cc';
 // @ts-ignore
-import { captureOneImage, runScene, sleep, testCase, testClass } from 'db://automation-framework/runtime/test-framework.mjs';
+import { beforeClass, testCase, testClass, srandom } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom } from '../common/utils';
 
-@runScene('particle-size')
-@testClass('ParticleSize')
+@testClass('ParticleSize', 'particle-size')
 export class ParticleSize {
     _dt = 6;
+
+    @beforeClass
+    async initData() {
+        srandom(director.getScene()!.name);
+    }
+
     @testCase
     async startPlay() {
         await screenshot_custom(this._dt);
