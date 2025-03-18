6a3777aa795a5208593ab6829d177f8f35e7ad31
lanbinqiang <39689887+W-MTZing@users.noreply.github.com>
2025-03-18 14:01:49 +0800

    discard api usage adjust (#913)

--- assets/cases/ui/22.webview/webview-ctrl.ts
+++ assets/cases/ui/22.webview/webview-ctrl.ts
@@ -15,21 +15,21 @@ export class WebviewCtrl extends Component {
     start () {
         // 隐藏不支持 video player 的平台
         switch (sys.platform) {
-            case sys.MACOS:
-            case sys.ALIPAY_MINI_GAME:
-            case sys.BYTEDANCE_MINI_GAME:
-            case sys.COCOSPLAY:
-            case sys.HUAWEI_QUICK_GAME:
-            case sys.OPPO_MINI_GAME:
-            case sys.VIVO_MINI_GAME:
-            case sys.MIGU_MINI_GAME:
-            case sys.HONOR_MINI_GAME:
-            case sys.XIAOMI_QUICK_GAME:
-            case sys.BAIDU_MINI_GAME:
-            case sys.WECHAT_GAME:
-            case sys.LINKSURE_MINI_GAME:
-            case sys.QTT_MINI_GAME:
-            case sys.WIN32:
+            case sys.Platform.MACOS:
+            case sys.Platform.ALIPAY_MINI_GAME:
+            case sys.Platform.BYTEDANCE_MINI_GAME:
+            case sys.Platform.COCOSPLAY:
+            case sys.Platform.HUAWEI_QUICK_GAME:
+            case sys.Platform.OPPO_MINI_GAME:
+            case sys.Platform.VIVO_MINI_GAME:
+            case sys.Platform.MIGU_MINI_GAME:
+            case sys.Platform.HONOR_MINI_GAME:
+            case sys.Platform.XIAOMI_QUICK_GAME:
+            case sys.Platform.BAIDU_MINI_GAME:
+            case sys.Platform.WECHAT_GAME:
+            case sys.Platform.LINKSURE_MINI_GAME:
+            case sys.Platform.QTT_MINI_GAME:
+            case sys.Platform.WIN32:
                 this.noSupport.active = true;
                 this.webview.node.active = false;
                 break;
