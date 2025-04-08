c2f343b6968663a2f1342c230e2e404368bb8f41
bofeng <bofeng.song@chukong-inc.com>
2025-04-08 10:23:40 +0800

    SpineTestCrash test case adds node destroy to verify crash issues

--- assets/cases/middleware/spine/SpineTestCrash.ts
+++ assets/cases/middleware/spine/SpineTestCrash.ts
@@ -26,8 +26,26 @@ export class SpineTestCrash extends Component {
     start() {
         const prefab = sp.SPINE_VERSION === '3.8' ? this.spineBoyPrefab : this.spineBoyPrefab_4_2;
         this.firstSpine.setCompleteListener((trackEntry) => {
-            const node = instantiate(prefab);
-            this.node.addChild(node);
+            // Create two nodes with Spine animations via Prefab, and set up/play the animations through code.
+            for (let i = 0; i < 2; i++) {
+                const node = instantiate(prefab);
+                node.setPosition(-200 + i * 150, 0)
+                node.name = 'spineBoy' + i;
+                this.node.addChild(node);
+                const spine = node.getComponent(sp.Skeleton);
+                // play animation
+                spine!.setAnimation(0, 'walk', false);
+            }
+
+            // Call destroy() on the last node after a delay of 1.5 seconds.
+            // This is to test the crash issue when destroying a node with a Spine animation.
+            this.scheduleOnce(() => {
+                const length = this.node.children.length;
+                const node = this.node.children[length - 1]
+                console.log('bf test node', node.name)
+                node.parent = null
+                node.destroy()
+            }, 1.5)
         });
     }
 
