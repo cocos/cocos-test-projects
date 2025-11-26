38204022d2ab51c20eb4779280270219cd0bca65
qiuguohua <qiuguohua111@126.com>
2025-11-26 17:45:56 +0800

    增加四个手柄的测试 (#931)
    * 增加四个手柄的测试
    
    * 增加手柄日志

--- assets/cases/event/system-event/gamepad-event.ts
+++ assets/cases/event/system-event/gamepad-event.ts
@@ -17,6 +17,12 @@ export class gamepad_event extends Component {
     @property(Toggle)
     public gamePad2: Toggle = null!;
 
+    @property(Toggle)
+    public gamePad3: Toggle = null!;
+
+    @property(Toggle)
+    public gamePad4: Toggle = null!;
+
     @property(ProgressBar)
     public L1: ProgressBar = null!;
     @property(ProgressBar)
@@ -78,6 +84,8 @@ export class gamepad_event extends Component {
 
         this.gamePad1.node.active = false;
         this.gamePad2.node.active = false;
+        this.gamePad3.node.active = false;
+        this.gamePad4.node.active = false;
 
         this._leftStickPos = this.L3.node.position.clone();
         this._rightStickPos = this.R3.node.position.clone();
@@ -88,6 +96,8 @@ export class gamepad_event extends Component {
         this.graphicsRight.stroke();
         this._gamepadArray.push({deviceId:-1, toggle: this.gamePad1});
         this._gamepadArray.push({deviceId:-1, toggle: this.gamePad2});
+        this._gamepadArray.push({deviceId:-1, toggle: this.gamePad3});
+        this._gamepadArray.push({deviceId:-1, toggle: this.gamePad4});
     }
 
     onDestroy () {
@@ -138,31 +148,58 @@ export class gamepad_event extends Component {
         }
 
         this.joystickInfo.string = '手柄ID: ' + gp.deviceId;
+        console.debug(`${this.joystickInfo.string}`);
         this.L1.progress =  gp.buttonL1.getValue();
+        console.debug(`    L1 : ${gp.buttonL1.getValue()}`);
+
         this.L2.progress = gp.buttonL2.getValue();
+        console.debug(`    L2 : ${gp.buttonL2.getValue()}`);
+
         this.L3.progress = gp.buttonL3.getValue();
+        console.debug(`    L3 : ${gp.buttonL3.getValue()}`);
+
         this.R1.progress = gp.buttonR1.getValue();
+        console.debug(`    R1 : ${gp.buttonR1.getValue()}`);
+
         this.R2.progress = gp.buttonR2.getValue();
+        console.debug(`    R2 : ${gp.buttonR2.getValue()}`);
+
         this.R3.progress = gp.buttonR3.getValue();
+        console.debug(`    R3 : ${gp.buttonR3.getValue()}`);
 
         this.BtnSouth.progress =  gp.buttonSouth.getValue();
+        console.debug(`    buttonSouth : ${gp.buttonSouth.getValue()}`);
         this.BtnEast.progress = gp.buttonEast.getValue();
+        console.debug(`    buttonEast : ${gp.buttonEast.getValue()}`);
         this.BtnWest.progress = gp.buttonWest.getValue();
+        console.debug(`    buttonWest : ${gp.buttonWest.getValue()}`);
         this.BtnNorth.progress = gp.buttonNorth.getValue();
+        console.debug(`    buttonNorth : ${gp.buttonNorth.getValue()}`);
 
         this.Up.progress =  gp.dpad.up.getValue();
+        console.debug(`    dpad.up : ${gp.dpad.up.getValue()}`);
         this.Down.progress = gp.dpad.down.getValue();
+        console.debug(`    dpad.down : ${gp.dpad.down.getValue()}`);
         this.Left.progress = gp.dpad.left.getValue();
+        console.debug(`    dpad.left : ${gp.dpad.left.getValue()}`);
         this.Right.progress = gp.dpad.right.getValue();
+        console.debug(`    dpad.right : ${gp.dpad.right.getValue()}`);
 
         this.Share.progress = gp.buttonShare.getValue();
+        console.debug(`    Share.progress : ${gp.buttonShare.getValue()}`);
+        
         this.Options.progress = gp.buttonOptions.getValue();
+        console.debug(`    Options.progress : ${gp.buttonOptions.getValue()}`);
         // this.Home.progress = gp.buttonHome.getValue();
 
         // this.TouchPad.progress = gp.buttonTouchPad.getValue();
 
         const ls = gp.leftStick.getValue();
+        console.debug(`    ls : ${gp.leftStick.getValue()}`);
+
         const rs = gp.rightStick.getValue();
+        console.debug(`    rs : ${gp.rightStick.getValue()}`);
+
         this.L3.node.setPosition(v3(this._leftStickPos.x + this._stickMoveDistance * ls.x, this._leftStickPos.y + this._stickMoveDistance * ls.y, 0));
         this.R3.node.setPosition(v3(this._rightStickPos.x + this._stickMoveDistance * rs.x, this._rightStickPos.y + this._stickMoveDistance * rs.y, 0));
     }
